44576ba1842f1b904b8e4c2a4f31deef263f8d15
Ragunath Jawahar <ragunath@redgreen.io>
2022-09-17 19:09:39 +0530

    build: setup shadow jar plugin

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,11 +1,36 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
   application
+  id("com.github.johnrengelman.shadow") version "6.0.0"
+}
+
+application {
+  applicationName = "lt"
+  mainClassName = "io.redgreen.lt.cli.MainKt"
+}
+
+tasks.withType<Jar> {
+  manifest {
+    attributes(
+      "Main-Class" to "io.redgreen.lt.cli.MainKt",
+    )
+  }
+}
+
+tasks {
+  named<ShadowJar>("shadowJar") {
+    archiveBaseName.set("lt")
+    mergeServiceFiles()
+  }
+}
+
+tasks {
+  build {
+    dependsOn(shadowJar)
+  }
 }
 
 dependencies {
   implementation("info.picocli:picocli:4.6.3")
-}
-
-application {
-  mainClass.set("io.redgreen.lt.cli.MainKt")
 }

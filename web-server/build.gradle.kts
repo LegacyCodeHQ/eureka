e9567d69004f1767bbe048bbe0cd43befc2ac9f8
Ragunath Jawahar <ragunath@redgreen.io>
2022-10-07 15:19:47 +0530

    build: add 'bytecode-scanner' and 'filesystem' to 'web-server'

--- web-server/build.gradle.kts
+++ web-server/build.gradle.kts
@@ -1,4 +1,7 @@
 dependencies {
+  implementation(project(":bytecode-scanner"))
+  implementation(project(":filesystem"))
+
   implementation("io.ktor:ktor-server-netty:2.1.2")
   implementation("io.ktor:ktor-server-websockets:2.1.2")
 }

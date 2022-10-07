0c1f077ae93c7cfac8e0526c6d47b9db1a91f997
Ragunath Jawahar <ragunath@redgreen.io>
2022-10-07 12:55:48 +0530

    feat: setup a `/ping` web socket endpoint

--- web-server/build.gradle.kts
+++ web-server/build.gradle.kts
@@ -1,3 +1,4 @@
 dependencies {
   implementation("io.ktor:ktor-server-netty:2.1.2")
+  implementation("io.ktor:ktor-server-websockets:2.1.2")
 }
--- web-server/src/main/kotlin/io/redgreen/tumbleweed/web/TumbleweedServer.kt
+++ web-server/src/main/kotlin/io/redgreen/tumbleweed/web/TumbleweedServer.kt
@@ -1,12 +1,20 @@
 package io.redgreen.tumbleweed.web
 
+import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.application.install
 import io.ktor.server.engine.ApplicationEngine
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import io.ktor.server.websocket.WebSockets
+import io.ktor.server.websocket.pingPeriod
+import io.ktor.server.websocket.timeout
+import io.ktor.server.websocket.webSocket
+import io.ktor.websocket.Frame
+import java.time.Duration
 import org.slf4j.LoggerFactory
 
 class TumbleweedServer {
@@ -17,8 +25,13 @@ class TumbleweedServer {
     val port = 7070
     logger.info("Starting web server @ http://localhost:{}", port)
     webServer = embeddedServer(Netty, port = port) {
+      installWebSockets()
       routing {
         get("/") { call.respond("Hello, world!") }
+
+        webSocket("/ping") {
+          send(Frame.Text("Ping received!"))
+        }
       }
     }.start(wait = true)
   }
@@ -26,5 +39,14 @@ class TumbleweedServer {
   fun stop() {
     logger.info("Stopping web server…")
     webServer.stop(0, 0)
+  }
+
+  private fun Application.installWebSockets() {
+    install(WebSockets) {
+      pingPeriod = Duration.ofSeconds(15)
+      timeout = Duration.ofSeconds(15)
+      maxFrameSize = Long.MAX_VALUE
+      masking = false
+    }
   }
 }

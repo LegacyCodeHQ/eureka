f9f1403e37b5f988f74cc9d67e7810ff1f1208bb
Ragunath Jawahar <ragunath@redgreen.io>
2022-10-08 12:00:02 +0530

    refactor: move `ClassFileLocation` to test source set

--- web-server/src/test/kotlin/io/redgreen/tumbleweed/web/observablehq/ClassStructureJsonKtTest.kt
+++ web-server/src/test/kotlin/io/redgreen/tumbleweed/web/observablehq/ClassStructureJsonKtTest.kt
@@ -1,8 +1,8 @@
 package io.redgreen.tumbleweed.web.observablehq
 
-import io.redgreen.tumbleweed.ClassFileLocation
 import io.redgreen.tumbleweed.ClassScanner
 import io.redgreen.tumbleweed.samples.ClassWithMethodReadingField
+import java.io.File
 import org.approvaltests.JsonApprovals
 import org.junit.jupiter.api.Test
 
@@ -10,11 +10,11 @@ internal class ClassStructureJsonKtTest {
   @Test
   internal fun `serialize class structure as edge bundling graph JSON`() {
     // given
-    val methodReadingFieldClass = ClassFileLocation(
-      compiledClassesDirectory = "../bytecode-samples/build/classes/java/main",
-      fqClassName = ClassWithMethodReadingField::class.java.name,
+    val classFile = File(
+      "../bytecode-samples/build/classes/java/main/" +
+        ClassWithMethodReadingField::class.java.name.replace(".", "/") + ".class"
     )
-    val classStructure = ClassScanner.scan(methodReadingFieldClass.file)
+    val classStructure = ClassScanner.scan(classFile)
 
     // when & then
     JsonApprovals.verifyJson(classStructure.json)

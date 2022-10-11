1953c03f9941dda5b88d3ccd40cadd5d96d1b595
Ragunath Jawahar <ragunath@redgreen.io>
2022-10-11 21:43:26 +0530

    refactor: move functions to a different file

---
+++ cli/src/main/kotlin/io/redgreen/tumbleweed/cli/dev/ClassStructure.kt
@@ -0,0 +1,44 @@
+package io.redgreen.tumbleweed.cli.dev
+
+import io.redgreen.tumbleweed.ClassStructure
+import io.redgreen.tumbleweed.Member
+
+fun ClassStructure.check() {
+  val membersInClassStructure = (fields + methods).toSet()
+  val membersInRelationship = relationships.flatMap { listOf(it.source, it.target) }.toSet()
+  val fqClassName = "$`package`.$className"
+
+  if (membersInClassStructure != membersInRelationship) {
+    val missingMembers = membersInRelationship - membersInClassStructure
+    val membersWithoutRelationships = membersInClassStructure - membersInRelationship
+    printReport(missingMembers, membersWithoutRelationships, fqClassName)
+  } else {
+    println("No inconsistencies found in '$fqClassName'")
+  }
+}
+
+private fun printReport(
+  missingMembers: Set<Member>,
+  membersWithoutRelationships: Set<Member>,
+  fqClassName: String,
+) {
+  println("Inconsistencies found in '$fqClassName'")
+
+  println("- Missing class members")
+  if (missingMembers.isEmpty()) {
+    println("    -  None")
+  } else {
+    missingMembers.forEachIndexed { idx, it ->
+      println("    ${idx + 1}. $it")
+    }
+  }
+
+  println("- Members without relationships")
+  if (membersWithoutRelationships.isEmpty()) {
+    println("    -  None")
+  } else {
+    membersWithoutRelationships.forEachIndexed { idx, it ->
+      println("    ${idx + 1}. ${it.signature}")
+    }
+  }
+}
--- cli/src/main/kotlin/io/redgreen/tumbleweed/cli/dev/JsonCommand.kt
+++ cli/src/main/kotlin/io/redgreen/tumbleweed/cli/dev/JsonCommand.kt
@@ -1,8 +1,6 @@
 package io.redgreen.tumbleweed.cli.dev
 
 import io.redgreen.tumbleweed.ClassScanner
-import io.redgreen.tumbleweed.ClassStructure
-import io.redgreen.tumbleweed.Member
 import io.redgreen.tumbleweed.filesystem.CompiledClassFileFinder
 import io.redgreen.tumbleweed.web.observablehq.json
 import java.io.File
@@ -47,46 +45,6 @@ class JsonCommand : Runnable {
       classStructure.check()
     } else {
       println(classStructure.json)
-    }
-  }
-
-  private fun ClassStructure.check() {
-    val membersInClassStructure = (fields + methods).toSet()
-    val membersInRelationship = relationships.flatMap { listOf(it.source, it.target) }.toSet()
-    val fqClassName = "$`package`.$className"
-
-    if (membersInClassStructure != membersInRelationship) {
-      val missingMembers = membersInRelationship - membersInClassStructure
-      val membersWithoutRelationships = membersInClassStructure - membersInRelationship
-      printReport(missingMembers, membersWithoutRelationships, fqClassName)
-    } else {
-      println("No inconsistencies found in '$fqClassName'")
-    }
-  }
-
-  private fun printReport(
-    missingMembers: Set<Member>,
-    membersWithoutRelationships: Set<Member>,
-    fqClassName: String,
-  ) {
-    println("Inconsistencies found in '$fqClassName'")
-
-    println("- Missing class members")
-    if (missingMembers.isEmpty()) {
-      println("    -  None")
-    } else {
-      missingMembers.forEachIndexed { idx, it ->
-        println("    ${idx + 1}. $it")
-      }
-    }
-
-    println("- Members without relationships")
-    if (membersWithoutRelationships.isEmpty()) {
-      println("    -  None")
-    } else {
-      membersWithoutRelationships.forEachIndexed { idx, it ->
-        println("    ${idx + 1}. ${it.signature}")
-      }
     }
   }
 }

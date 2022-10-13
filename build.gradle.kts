3e45bac1ef920bb8fe0e4c5db90aae1d23d925ac
Ragunath Jawahar <ragunath@redgreen.io>
2022-10-13 08:33:36 +0530

    build: bump JUnit

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,9 +38,9 @@ subprojects {
 
     // testing
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 
     testImplementation("com.google.truth:truth:1.1.3")
 

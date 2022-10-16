ef124ced3f894631c49632f849bde2d933417ec2
Ragunath Jawahar <ragunath@redgreen.io>
2022-10-16 13:28:25 +0530

    chore: suppress unused  warning

--- bytecode-samples/src/main/java/io/redgreen/tumbleweed/samples/AnotherFindMeJavaClass.java
+++ bytecode-samples/src/main/java/io/redgreen/tumbleweed/samples/AnotherFindMeJavaClass.java
@@ -1,9 +1,8 @@
 package io.redgreen.tumbleweed.samples;
 
+@SuppressWarnings("unused")
 public class AnotherFindMeJavaClass {
-
   public void invoke() {
     FindMeJavaClass.main(new String[]{"Sample Arg"});
   }
-
 }

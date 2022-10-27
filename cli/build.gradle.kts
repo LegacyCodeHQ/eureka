2075e6f54202c6c811217d83bc72530b7fe72258
Ragunath Jawahar <ragunath@redgreen.io>
2022-10-27 21:35:59 +0530

    build: prepare next iteration

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jreleaser.model.Active
 
 @Suppress("PropertyName")
-val CLI_VERSION = "0.19.0"
+val CLI_VERSION = "0.20.0-SNAPSHOT"
 
 plugins {
   application

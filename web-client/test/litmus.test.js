b6cc38a3cc0ae49f4f3bb9b44b65b2e90ebf4d77
Ragunath Jawahar <ragunath@redgreen.io>
2022-10-28 20:34:28 +0530

    test: add litmus test for Approvals.Js

--- web-client/test/litmus.test.js
+++ web-client/test/litmus.test.js
@@ -1,9 +1,16 @@
 const { should } = require('chai');
 
+const approvals = require('approvals');
+const approvalsDir = 'test/approved/litmus';
+
 should();
 
 describe('a test environment', () => {
   it('should have mocha setup', () => {
     true.should.be.true; // eslint-disable-line
+  });
+
+  it('should have approvals setup', function () {
+    approvals.verify(approvalsDir, this.test.title, 'Hello, world!');
   });
 });

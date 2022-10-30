2951766e235ae0ebb36ecf0bc6e247af130426da
Ragunath Jawahar <ragunath@redgreen.io>
2022-10-30 14:51:07 +0530

    build: bump ktor

--- web-server/build.gradle.kts
+++ web-server/build.gradle.kts
@@ -2,8 +2,8 @@ dependencies {
   implementation(project(":bytecode-scanner"))
   implementation(project(":filesystem"))
 
-  implementation("io.ktor:ktor-server-netty:2.1.2")
-  implementation("io.ktor:ktor-server-websockets:2.1.2")
+  implementation("io.ktor:ktor-server-netty:2.1.3")
+  implementation("io.ktor:ktor-server-websockets:2.1.3")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
 

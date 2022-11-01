b0cc43cc1e01963773017d5a662f15844076b554
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-01 16:22:22 +0530

    feat: add function to sort types

--- web-client/src/domain/types.test.js
+++ web-client/src/domain/types.test.js
@@ -28,3 +28,27 @@ describe('a qualified type name', () => {
     actual.should.equal('ThisClassIsInThe...PackageButIsLong');
   });
 });
+
+describe('a set of types', () => {
+  it('should be sorted in alphabetical order', function () {
+    const types = [
+      'org.thoughtcrime.securesms.mediapreview.MediaPreviewFragment$Events',
+      'org.thoughtcrime.securesms.conversation.mutiselect.forward.MultiselectForwardBottomSheet$Callback',
+      'org.thoughtcrime.securesms.stories.StorySlateView$Callback',
+      'org.thoughtcrime.securesms.stories.viewer.text.StoryTextPostPreviewFragment$Callback',
+      'org.thoughtcrime.securesms.stories.viewer.reply.StoriesSharedElementCrossFaderView$Callback',
+    ];
+    const actual = sortTypes(types);
+    actual.should.deep.equal([
+      'org.thoughtcrime.securesms.conversation.mutiselect.forward.MultiselectForwardBottomSheet$Callback',
+      'org.thoughtcrime.securesms.mediapreview.MediaPreviewFragment$Events',
+      'org.thoughtcrime.securesms.stories.StorySlateView$Callback',
+      'org.thoughtcrime.securesms.stories.viewer.reply.StoriesSharedElementCrossFaderView$Callback',
+      'org.thoughtcrime.securesms.stories.viewer.text.StoryTextPostPreviewFragment$Callback',
+    ]);
+  });
+});
+
+function sortTypes(types) {
+  return types.sort();
+}

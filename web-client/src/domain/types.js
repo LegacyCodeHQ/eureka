e0af724360357de1082c6b1ee9531ecd4708e529
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-01 16:06:21 +0530

    refactor: move function

---
+++ web-client/src/domain/types.js
@@ -0,0 +1,18 @@
+export function shorten(className) {
+  const threshold = 32;
+  const parts = className.split('.');
+  const isLongClassName = className.length > threshold;
+  const isInDefaultPackage = parts.length === 1;
+
+  if (isLongClassName && !isInDefaultPackage) {
+    let shortenedPackageName = parts.slice(0, parts.length - 1)
+      .map(part => part[0])
+      .join('.');
+    let simpleClassName = parts.slice(parts.length - 1);
+    return shortenedPackageName + '.' + simpleClassName;
+  } else if (isLongClassName && isInDefaultPackage) {
+    let charsToKeep = threshold / 2;
+    return className.slice(0, charsToKeep) + '...' + className.slice(className.length - charsToKeep);
+  }
+  return className;
+}
--- web-client/src/domain/types.test.js
+++ web-client/src/domain/types.test.js
@@ -1,24 +1,7 @@
 require('chai')
   .should();
 
-function shorten(className) {
-  const threshold = 32;
-  const parts = className.split('.');
-  const isLongClassName = className.length > threshold;
-  const isInDefaultPackage = parts.length === 1;
-
-  if (isLongClassName && !isInDefaultPackage) {
-    let shortenedPackageName = parts.slice(0, parts.length - 1)
-      .map(part => part[0])
-      .join('.');
-    let simpleClassName = parts.slice(parts.length - 1);
-    return shortenedPackageName + '.' + simpleClassName;
-  } else if (isLongClassName && isInDefaultPackage) {
-    let charsToKeep = threshold / 2;
-    return className.slice(0, charsToKeep) + '...' + className.slice(className.length - charsToKeep);
-  }
-  return className;
-}
+const { shorten } = require('./types.js');
 
 describe('a qualified type name', () => {
   it('should return the same name if it is in the default package', function () {

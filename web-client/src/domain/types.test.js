bdc86027da5a12700ad682b2e953b18e335caff7
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-01 15:18:51 +0530

    refactor: rename tests

--- web-client/src/domain/types.test.js
+++ web-client/src/domain/types.test.js
@@ -4,14 +4,14 @@ function shorten(className) {
   return className;
 }
 
-describe('a class name', () => {
+describe('a qualified type name', () => {
   it('should return the same name if it is in the default package', function () {
     const className = 'ClassName'
     const actual = shorten(className)
     actual.should.equal(className)
   });
 
-  it('should return the same name if it is within the character limit', function () {
+  it('should return the same name if it is within 32 chars', function () {
     const className = 'com.example.ClassName'
     const actual = shorten(className)
     actual.should.equal(className)

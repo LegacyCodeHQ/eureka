2f1d864ae3336a73c3933ada90509c6c8d0bf645
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-01 15:27:41 +0530

    feat: shorten package name for qualified type names

--- web-client/src/domain/types.test.js
+++ web-client/src/domain/types.test.js
@@ -1,6 +1,15 @@
-require('chai').should()
+require('chai')
+  .should();
 
 function shorten(className) {
+  if (className.length > 32) {
+    let parts = className.split('.');
+    let shortenedPackageName = parts.slice(0, parts.length - 1)
+      .map(part => part[0])
+      .join('.');
+    let simpleClassName = parts.slice(parts.length - 1);
+    return shortenedPackageName + '.' + simpleClassName;
+  }
   return className;
 }
 
@@ -15,5 +24,11 @@ describe('a qualified type name', () => {
     const className = 'com.example.ClassName'
     const actual = shorten(className)
     actual.should.equal(className)
+  });
+
+  it('should return a shortened package name if it is beyond 32 chars', function () {
+    const className = 'com.example.viz.graphs.bundling.ClassName';
+    const actual = shorten(className);
+    actual.should.equal('c.e.v.g.b.ClassName');
   });
 });

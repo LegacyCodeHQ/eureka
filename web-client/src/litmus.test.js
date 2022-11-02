0a37036f0681102553c959c4ac8681b3e875d233
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-02 07:01:36 +0530

    test: snapshot litmus test

--- web-client/src/litmus.test.js
+++ web-client/src/litmus.test.js
@@ -4,4 +4,8 @@ describe('a test environment', () => {
   it('should have a test runner', () => {
     true.should.be.true;
   });
+
+  it('should have snapshot testing', () => {
+    expect('Hello, world!').toMatchSnapshot();
+  });
 });

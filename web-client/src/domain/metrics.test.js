e2639c98ce8c715c737013691688f43f69d821f8
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-03 19:13:27 +0530

    feat: calculate stability for a maximally unstable member

--- web-client/src/domain/metrics.test.js
+++ web-client/src/domain/metrics.test.js
@@ -2,7 +2,7 @@ require('chai')
   .should();
 
 function instability(dependencies, dependents) {
-  return 0.0;
+  return dependencies / (dependencies + dependents);
 }
 
 describe('instability (I)', () => {
@@ -11,5 +11,12 @@ describe('instability (I)', () => {
     let dependents = 5;
     let i = instability(dependencies, dependents);
     i.should.equal(0.0);
+  });
+
+  it('should calculate metrics for a maximally unstable member', function () {
+    let dependencies = 5;
+    let dependents = 0;
+    let i = instability(dependencies, dependents);
+    i.should.equal(1.0);
   });
 });

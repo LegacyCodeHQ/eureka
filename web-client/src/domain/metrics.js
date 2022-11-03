94cd0e816af408cd8691db60d67ead3a81b4126f
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-03 19:41:39 +0530

    feat: calculate effort for extracting members

--- web-client/src/domain/metrics.js
+++ web-client/src/domain/metrics.js
@@ -2,3 +2,13 @@ export function instability(dependencies, dependents) {
   let i = dependencies / (dependencies + dependents);
   return Math.round((i + Number.EPSILON) * 100) / 100;
 }
+
+/**
+ * Experimental: Calculates the effort metric for a given member.
+ * @param dependencies
+ * @param dependents
+ * @returns {*} effort
+ */
+export function effort(dependencies, dependents) {
+  return dependencies + (dependents / 2);
+}
--- web-client/src/domain/metrics.test.js
+++ web-client/src/domain/metrics.test.js
@@ -3,6 +3,7 @@ require('chai')
 
 const {
   instability,
+  effort,
 } = require('./metrics.js');
 
 describe('instability (I)', () => {
@@ -32,5 +33,35 @@ describe('instability (I)', () => {
     let dependents = 5;
     let i = instability(dependencies, dependents);
     i.should.equal(0.67);
+  });
+});
+
+describe('effort (E)', () => {
+  it('should calculate effort for a maximally stable member', function () {
+    let dependencies = 0;
+    let dependents = 5;
+    let e = effort(dependencies, dependents);
+    e.should.equal(2.5);
+  });
+
+  it('should calculate effort for a maximally unstable member', function () {
+    let dependencies = 5;
+    let dependents = 0;
+    let e = effort(dependencies, dependents);
+    e.should.equal(5);
+  });
+
+  it('should calculate effort for a member with equal dependencies and dependents', function () {
+    let dependencies = 5;
+    let dependents = 5;
+    let e = effort(dependencies, dependents);
+    e.should.equal(7.5);
+  });
+
+  it('should calculate effort for a member with more dependencies than dependents', function () {
+    let dependencies = 10;
+    let dependents = 5;
+    let e = effort(dependencies, dependents);
+    e.should.equal(12.5);
   });
 });

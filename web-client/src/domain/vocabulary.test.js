41bb245dd387cd507d391d2d4ec610daf5252773
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-04 18:42:52 +0530

    refactor: return an object, instead of an array

--- web-client/src/domain/vocabulary.test.js
+++ web-client/src/domain/vocabulary.test.js
@@ -5,7 +5,10 @@ function tokenize(field) {
   let parts = field.split(' ');
   let type = parts[0];
   let identifier = parts[1];
-  return [type].concat(splitIdentifier(identifier));
+  return {
+    type: type,
+    words: splitIdentifier(identifier),
+  };
 }
 
 function splitIdentifier(identifier) {
@@ -18,21 +21,38 @@ function splitIdentifier(identifier) {
 describe('tokenization', () => {
   it('should tokenize a simple field', () => {
     const tokens = tokenize('int x');
-    tokens.should.deep.equal(['int', 'x']);
+    tokens.should.deep.equal({
+      type: 'int',
+      words: ['x'],
+    });
   });
 
   it('should tokenize a field in camel case', function () {
     const actual = tokenize('int xCamelCase');
-    actual.should.deep.equal(['int', 'x', 'Camel', 'Case']);
+    actual.should.deep.equal({
+      type: 'int',
+      words: ['x', 'Camel', 'Case'],
+    });
   });
 
   it('should tokenize a field in snake case', function () {
     const actual = tokenize('int x_snake_case');
-    actual.should.deep.equal(['int', 'x', 'snake', 'case']);
+    actual.should.deep.equal({
+      type: 'int',
+      words: ['x',
+        'snake',
+        'case'
+      ]
+    });
   });
 
   it('should tokenize constants', function () {
     const actual = tokenize('int X_CONSTANT');
-    actual.should.deep.equal(['int', 'X', 'CONSTANT']);
+    actual.should.deep.equal({
+      type: 'int',
+      words: ['X',
+        'CONSTANT'
+      ]
+    });
   });
 });

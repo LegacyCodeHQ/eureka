20c588ead3335a35c19b9adc04c10a78ab6c453d
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-04 18:34:43 +0530

    feat: tokenize a camel case identifier

--- web-client/src/domain/vocabulary.test.js
+++ web-client/src/domain/vocabulary.test.js
@@ -2,12 +2,24 @@ require('chai')
   .should();
 
 function tokenize(field) {
-  return field.split(" ");
+  let parts = field.split(' ');
+  let type = parts[0];
+  let identifier = parts[1];
+  return [type].concat(splitIdentifier(identifier));
+}
+
+function splitIdentifier(identifier) {
+  return identifier.split(/(?=[A-Z])/);
 }
 
 describe('tokenization', () => {
   it('should tokenize a simple field', () => {
     const tokens = tokenize('int x');
     tokens.should.deep.equal(['int', 'x']);
+  });
+
+  it('should tokenize a field in camel case', function () {
+    const actual = tokenize('int xCamelCase');
+    actual.should.deep.equal(['int', 'x', 'Camel', 'Case']);
   });
 });

12e033f02eb2157e2e74be44150f5940f4ac4b23
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-10 18:35:53 +0530

    test: verify support for `double` constants

--- bytecode/samples/src/main/java/io/redgreen/tumbleweed/samples/Constants.java
+++ bytecode/samples/src/main/java/io/redgreen/tumbleweed/samples/Constants.java
@@ -4,10 +4,12 @@ public class Constants {
   private static final String CONSTANT = "Hello, World!";
   private static final int NUMBER = 42;
   private static final long LONG_NUMBER = 42L;
+  private static final double DOUBLE = 4.2;
 
   public static void main(String[] args) {
     System.out.println(CONSTANT);
     System.out.println(NUMBER);
     System.out.println(LONG_NUMBER);
+    System.out.println(DOUBLE);
   }
 }

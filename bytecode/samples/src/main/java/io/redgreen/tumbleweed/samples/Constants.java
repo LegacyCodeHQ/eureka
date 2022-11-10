b454f901eafcf013f4781ec0670c1341e5c97a3a
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-10 18:34:17 +0530

    test: verify support for `long` constants

--- bytecode/samples/src/main/java/io/redgreen/tumbleweed/samples/Constants.java
+++ bytecode/samples/src/main/java/io/redgreen/tumbleweed/samples/Constants.java
@@ -3,9 +3,11 @@ package io.redgreen.tumbleweed.samples;
 public class Constants {
   private static final String CONSTANT = "Hello, World!";
   private static final int NUMBER = 42;
+  private static final long LONG_NUMBER = 42L;
 
   public static void main(String[] args) {
     System.out.println(CONSTANT);
     System.out.println(NUMBER);
+    System.out.println(LONG_NUMBER);
   }
 }

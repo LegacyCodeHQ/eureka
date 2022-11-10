92cfe1fdf893601de4cb7c21dedab9684858d366
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-10 18:39:04 +0530

    refactor: rename constant fields

--- bytecode/samples/src/main/java/io/redgreen/tumbleweed/samples/Constants.java
+++ bytecode/samples/src/main/java/io/redgreen/tumbleweed/samples/Constants.java
@@ -1,18 +1,18 @@
 package io.redgreen.tumbleweed.samples;
 
 public class Constants {
-  private static final String CONSTANT = "Hello, World!";
-  private static final int NUMBER = 42;
-  private static final long LONG_NUMBER = 42L;
+  private static final String STRING = "Hello, World!";
+  private static final int INT = 42;
+  private static final long LONG = 42L;
   private static final double DOUBLE = 4.2;
-  private static final double FLOAT = 4.2F;
+  private static final float FLOAT = 4.2F;
   private static final int CHAR = 'A';
-  private static final int SHORT = 8;
+  private static final short SHORT = 8;
 
   public static void main(String[] args) {
-    System.out.println(CONSTANT);
-    System.out.println(NUMBER);
-    System.out.println(LONG_NUMBER);
+    System.out.println(STRING);
+    System.out.println(INT);
+    System.out.println(LONG);
     System.out.println(DOUBLE);
     System.out.println(FLOAT);
     System.out.println(CHAR);

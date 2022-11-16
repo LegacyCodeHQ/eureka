77b3624785358be52ec0130ab0e399aaf3cd9d1f
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-16 19:48:30 +0530

    refactor: new `Metrics` class

--- web-client/src/domain/metrics.test.ts
+++ web-client/src/domain/metrics.test.ts
@@ -2,36 +2,35 @@ require('chai')
   .should();
 
 const {
-  instability,
-  effort,
+  Metrics,
 } = require('./metrics');
 
 describe('instability (I)', () => {
   it('should calculate metrics for a maximally stable member', function () {
     let dependencies = 0;
     let dependents = 5;
-    let i = instability(dependencies, dependents);
+    let i = Metrics.instability(dependencies, dependents);
     i.should.equal(0.0);
   });
 
   it('should calculate metrics for a maximally unstable member', function () {
     let dependencies = 5;
     let dependents = 0;
-    let i = instability(dependencies, dependents);
+    let i = Metrics.instability(dependencies, dependents);
     i.should.equal(1.0);
   });
 
   it('should calculate metrics for a member with equal dependencies and dependents', function () {
     let dependencies = 5;
     let dependents = 5;
-    let i = instability(dependencies, dependents);
+    let i = Metrics.instability(dependencies, dependents);
     i.should.equal(0.5);
   });
 
   it('should calculate metrics for a member with more dependencies than dependents', function () {
     let dependencies = 10;
     let dependents = 5;
-    let i = instability(dependencies, dependents);
+    let i = Metrics.instability(dependencies, dependents);
     i.should.equal(0.67);
   });
 });
@@ -40,28 +39,28 @@ describe('effort (E)', () => {
   it('should calculate effort for a maximally stable member', function () {
     let dependencies = 0;
     let dependents = 5;
-    let e = effort(dependencies, dependents);
+    let e = Metrics.effort(dependencies, dependents);
     e.should.equal(2.5);
   });
 
   it('should calculate effort for a maximally unstable member', function () {
     let dependencies = 5;
     let dependents = 0;
-    let e = effort(dependencies, dependents);
+    let e = Metrics.effort(dependencies, dependents);
     e.should.equal(5);
   });
 
   it('should calculate effort for a member with equal dependencies and dependents', function () {
     let dependencies = 5;
     let dependents = 5;
-    let e = effort(dependencies, dependents);
+    let e = Metrics.effort(dependencies, dependents);
     e.should.equal(7.5);
   });
 
   it('should calculate effort for a member with more dependencies than dependents', function () {
     let dependencies = 10;
     let dependents = 5;
-    let e = effort(dependencies, dependents);
+    let e = Metrics.effort(dependencies, dependents);
     e.should.equal(12.5);
   });
 });
--- web-client/src/domain/metrics.ts
+++ web-client/src/domain/metrics.ts
@@ -1,14 +1,16 @@
-export function instability(dependencies, dependents) {
-  let i = dependencies / (dependencies + dependents);
-  return Math.round((i + Number.EPSILON) * 100) / 100;
-}
+export class Metrics {
+  public static instability(dependencies: number, dependents: number): number {
+    let i = dependencies / (dependencies + dependents);
+    return Math.round((i + Number.EPSILON) * 100) / 100;
+  }
 
-/**
- * Experimental: Calculates the effort metric for a given member.
- * @param dependencies
- * @param dependents
- * @returns {*} effort
- */
-export function effort(dependencies, dependents) {
-  return dependencies + (dependents / 2);
+  /**
+   * Experimental: Calculates the effort metric for a given member.
+   * @param dependencies
+   * @param dependents
+   * @returns {*} effort
+   */
+  public static effort(dependencies: number, dependents: number): number {
+    return dependencies + (dependents / 2);
+  }
 }

4d47fbdf56f60263aca0ad5346512a07b3301bd1
Ragunath Jawahar <ragunath@redgreen.io>
2022-12-01 16:18:19 +0530

    build: add a task to clone git repo

--- vcs/build.gradle.kts
+++ vcs/build.gradle.kts
@@ -2,3 +2,45 @@ dependencies {
   implementation("org.buildobjects:jproc:2.8.2")
   implementation("io.arrow-kt:arrow-core:1.1.2")
 }
+
+task("cloneTestRepo") {
+  fun cloneGitRepo(repoDestination: File, repoUrl: String) {
+    val process = Runtime.getRuntime().exec(
+      arrayOf(
+        "git",
+        "clone",
+        repoUrl,
+        repoDestination.canonicalPath,
+      )
+    )
+    process.waitFor()
+  }
+
+  fun checkoutCommit(repoDestination: File, commitHash: String) {
+    val process = Runtime.getRuntime().exec(
+      arrayOf(
+        "git",
+        "-C",
+        repoDestination.canonicalPath,
+        "checkout",
+        commitHash,
+      )
+    )
+    process.waitFor()
+  }
+
+  val testDataDirectory = File(System.getProperty("user.home")).resolve(".tumbleweed-test-data")
+  if (!testDataDirectory.exists()) {
+    println("Test data directory does not exist, creating...")
+    testDataDirectory.mkdir()
+  }
+
+  val testRepoDirectory = testDataDirectory.resolve("simple-android")
+  if (!testRepoDirectory.exists()) {
+    testRepoDirectory.mkdir()
+    println("Cloning test repository...")
+
+    cloneGitRepo(testRepoDirectory, "https://github.com/ragunathjawahar/simple-android.git")
+    checkoutCommit(testRepoDirectory, "5eb413173505ceb287a7b0bfb27b698ed556c829")
+  }
+}

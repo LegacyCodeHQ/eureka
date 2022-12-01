4f31dcef97149265c4c373263aa7f54dec6cdd10
Ragunath Jawahar <ragunath@redgreen.io>
2022-12-01 14:56:53 +0530

    build: bump Jackson version

--- web-server/build.gradle.kts
+++ web-server/build.gradle.kts
@@ -5,7 +5,7 @@ dependencies {
   implementation("io.ktor:ktor-server-netty:2.1.3")
   implementation("io.ktor:ktor-server-websockets:2.1.3")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.0")
 
   testImplementation(project(":bytecode:samples"))
 }

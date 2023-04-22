c9650be7fa0379c37ba7360312e23e2944973465
Ragunath Jawahar <ragunath@redgreen.io>
2023-04-22 17:19:02 +0530

    build: add Mobius dependencies

--- web-server/build.gradle.kts
+++ web-server/build.gradle.kts
@@ -7,6 +7,9 @@ dependencies {
   implementation("io.ktor:ktor-server-websockets:2.1.3")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.0")
+  implementation("com.spotify.mobius:mobius-core:1.5.11")
+  implementation("com.spotify.mobius:mobius-rx3:1.5.11")
 
   testImplementation(project(":bytecode:samples"))
+  testImplementation("com.spotify.mobius:mobius-test:1.5.11")
 }

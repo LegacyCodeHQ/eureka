cb49a66176bd7be3c34604597ae11b6653464f8f
Ragunath Jawahar <ragunath@redgreen.io>
2023-04-22 20:06:40 +0530

    refactor: move logic from function to a class

--- filesystem/src/main/kotlin/io/redgreen/tumbleweed/filesystem/FileWatcher.kt
+++ filesystem/src/main/kotlin/io/redgreen/tumbleweed/filesystem/FileWatcher.kt
@@ -1,47 +1,58 @@
 package io.redgreen.tumbleweed.filesystem
 
-import com.sun.nio.file.SensitivityWatchEventModifier
+import com.sun.nio.file.SensitivityWatchEventModifier.HIGH
 import java.nio.file.ClosedWatchServiceException
 import java.nio.file.FileSystems
 import java.nio.file.Path
 import java.nio.file.StandardWatchEventKinds.ENTRY_CREATE
 import java.nio.file.StandardWatchEventKinds.ENTRY_DELETE
 import java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY
+import java.nio.file.WatchKey
 import java.nio.file.WatchService
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 class FileWatcher {
   private val logger = LoggerFactory.getLogger(FileWatcher::class.java)
+
   private val watchService: WatchService = FileSystems.getDefault().newWatchService()
+  private val eventsToWatchFor = arrayOf(ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE)
 
   fun startWatching(file: Path, onFileChanged: () -> Unit) {
-    var watchKey = file.parent.register(
-      watchService,
-      arrayOf(ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE),
-      SensitivityWatchEventModifier.HIGH
-    )
+    val watchKey = file.parent.register(watchService, eventsToWatchFor, HIGH)
     logger.info("Watching for file changes in {}", file.parent)
-
-    Thread {
-      while (true) {
-        val pollEvents = watchKey.pollEvents()
-        for (pollEvent in pollEvents) {
-          val fileWithEvent = pollEvent.context() as Path
-          logger.debug("WatchEvent: {} on {}", pollEvent.kind(), fileWithEvent)
-          onFileChanged()
-        }
-        watchKey.reset()
-        try {
-          watchKey = watchService.take()
-        } catch (e: ClosedWatchServiceException) {
-          logger.info("Stopped watching files", e)
-          break
-        }
-      }
-    }.start()
+    Thread(PollEventsRunnable(watchService, watchKey, onFileChanged)).start()
   }
 
   fun stopWatching() {
     watchService.close()
+  }
+}
+
+class PollEventsRunnable(
+  private val watchService: WatchService,
+  private val initialWatchKey: WatchKey,
+  private val onFileChanged: () -> Unit,
+) : Runnable {
+  private val logger: Logger = LoggerFactory.getLogger(PollEventsRunnable::class.java)
+
+  override fun run() {
+    var watchKey = initialWatchKey
+    while (true) {
+      val pollEvents = watchKey.pollEvents()
+      for (pollEvent in pollEvents) {
+        val affectedFile = pollEvent.context() as Path
+        logger.debug("WatchEvent: {} on {}", pollEvent.kind(), affectedFile)
+        onFileChanged()
+      }
+      watchKey.reset()
+
+      try {
+        watchKey = watchService.take()
+      } catch (e: ClosedWatchServiceException) {
+        logger.info("Stopped watching files", e)
+        break
+      }
+    }
   }
 }

38a0c64d8f6f5e2dc19c6110973bd315e0bfaf46
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-15 16:27:03 +0530

    refactor: move sample data to a separate file

--- src/vocabulary/VocabularyPanel.tsx
+++ src/vocabulary/VocabularyPanel.tsx
@@ -1,42 +1,7 @@
 import React, {useState} from 'react';
 import TokenStatsTable from "./TokenStatsTable";
 import {TokenStat} from "./model/TokenStat";
-import {Token} from "./model/Token";
-
-let typeTokenStats = [
-  new TokenStat(new Token("void"), 25),
-  new TokenStat(new Token("Attachment"), 10),
-  new TokenStat(new Token("boolean"), 9),
-  new TokenStat(new Token("String"), 4),
-  new TokenStat(new Token("Poll"), 4),
-  new TokenStat(new Token("int"), 4),
-  new TokenStat(new Token("List"), 3),
-  new TokenStat(new Token("View"), 3),
-  new TokenStat(new Token("Status"), 2),
-  new TokenStat(new Token("Bundle"), 2),
-  new TokenStat(new Token("Activity"), 2),
-  new TokenStat(new Token("Shuffle"), 2),
-  new TokenStat(new Token("FragmentManager"), 2),
-  new TokenStat(new Token("TransactionManager"), 2),
-  new TokenStat(new Token("ArrayList"), 2),
-  new TokenStat(new Token("Toolbar"), 1),
-  new TokenStat(new Token("TextView"), 1),
-  new TokenStat(new Token("ProgressBar"), 1),
-  new TokenStat(new Token("SelfUpdateStateChangedEvent"), 1),
-  new TokenStat(new Token("UpdateState"), 1),
-  new TokenStat(new Token("Holder"), 1),
-  new TokenStat(new Token("StatusCreatedEvent"), 1),
-  new TokenStat(new Token("Configuration"), 1),
-  new TokenStat(new Token("MenuItem"), 1),
-  new TokenStat(new Token("Menu"), 1),
-  new TokenStat(new Token("MenuInflater"), 1),
-  new TokenStat(new Token("APIRequest"), 1),
-  new TokenStat(new Token("AnimatorSet"), 1),
-  new TokenStat(new Token("Button"), 1),
-  new TokenStat(new Token("ImageView"), 1),
-  new TokenStat(new Token("CheckBox"), 1),
-  new TokenStat(new Token("ImageButton"), 1),
-];
+import {typeTokenStats} from "./model/SampleData";
 
 const VocabularyPanel = () => {
   const [activeTab, setActiveTab] = useState('types');

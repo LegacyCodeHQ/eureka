4335a236a23f56384f1bef46bb4587fa1cdd942f
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-16 14:09:57 +0530

    chore!!: remove unused css file

--- src/viz/EdgeBundlingGraph.tsx
+++ src/viz/EdgeBundlingGraph.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useRef} from "react";
 import * as d3 from "d3";
-import "./EdgeBundlingGraph.css";
 import {createRoot, GraphData, line} from "./Model";
 
 interface EdgeBundlingGraphProps {

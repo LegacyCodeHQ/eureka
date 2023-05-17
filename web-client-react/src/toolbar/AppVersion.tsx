bb637e34575ec0041e14e40d2c7d66a256b95f4a
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-17 19:46:21 +0530

    feat: show app version on the toolbar

--- web-client-react/src/toolbar/AppVersion.tsx
+++ web-client-react/src/toolbar/AppVersion.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './AppVersion.css';
 
 const AppVersion: React.FC = () => {
-  return <span className="app-version">v0.41.0-DEMO</span>;
+  if (process.env.NODE_ENV === 'development') {
+    return <AppVersionDev />;
+  }
+  return <AppVersionProd />;
+};
+
+const AppVersionProd: React.FC = () => {
+  const [versionName, setVersionName] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch('http://localhost:7070/version')
+      .then((response) => response.text())
+      .then((data) => {
+        setVersionName(data);
+      });
+  }, []);
+
+  return <span className="app-version">v{versionName}</span>;
+};
+
+const AppVersionDev: React.FC = () => {
+  return <span className="app-version">v2023.1.0-DEMO</span>;
 };
 
 export default AppVersion;

4e52014da4dbddd0199acce459a23ea5bbee439f
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-17 16:34:50 +0530

    ui: add toolbar with product name and class name

--- web-client-react/src/App.tsx
+++ web-client-react/src/App.tsx
@@ -7,14 +7,20 @@ import { GraphData } from './viz/model/GraphData';
 
 function App() {
   return (
-    <GraphDataSource>
-      {(data: GraphData | null) => (
-        <div className="App">
-          <div className="viz">{data && <EdgeBundlingGraph data={data} />}</div>
-          {data && <VocabularyPanel data={data} />}
-        </div>
-      )}
-    </GraphDataSource>
+    <div>
+      <div className="toolbar">
+        <span className="product-name">TWD</span>
+        <span className="class-name">StoryViewerFragment</span>
+      </div>
+      <GraphDataSource>
+        {(data: GraphData | null) => (
+          <div className="App">
+            <div className="viz">{data && <EdgeBundlingGraph data={data} />}</div>
+            {data && <VocabularyPanel data={data} />}
+          </div>
+        )}
+      </GraphDataSource>
+    </div>
   );
 }
 

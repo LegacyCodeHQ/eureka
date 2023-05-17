51b0b5391ae16d8520f329f28e4c7106e52b8780
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-17 17:23:25 +0530

    feat: update page title

--- web-client-react/src/App.tsx
+++ web-client-react/src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import VocabularyPanel from './vocabulary/VocabularyPanel';
 import EdgeBundlingGraph from './viz/EdgeBundlingGraph';
 import GraphDataSource from './source/GraphDataSource';
 import { GraphData } from './viz/model/GraphData';
 import SimpleJvmClassName from './SimpleJvmClassName';
+import { getSimpleClassName } from './types/Functions';
+import { ClassInfo } from './viz/model/ClassInfo';
 
 function App() {
+  function makeTitle(classInfo: ClassInfo | undefined): string {
+    return 'TWD ' + getSimpleClassName(classInfo ? classInfo.name : '');
+  }
+
+  function setTitle(title: string) {
+    useEffect(() => {
+      document.title = title;
+    }, [title]);
+  }
+
   return (
     <GraphDataSource>
-      {(data: GraphData | null) => (
-        <div>
-          <div className="toolbar">
-            <span className="product-name">TWD</span>
-            {data && <SimpleJvmClassName classInfo={data.meta.classInfo} />}
+      {(data: GraphData | null) => {
+        setTitle(makeTitle(data?.meta.classInfo));
+
+        return (
+          <div>
+            <div className="toolbar">
+              <span className="product-name">TWD</span>
+              {data && <SimpleJvmClassName classInfo={data.meta.classInfo} />}
+            </div>
+            <div className="App">
+              <div className="viz">{data && <EdgeBundlingGraph data={data} />}</div>
+              {data && <VocabularyPanel data={data} />}
+            </div>
           </div>
-          <div className="App">
-            <div className="viz">{data && <EdgeBundlingGraph data={data} />}</div>
-            {data && <VocabularyPanel data={data} />}
-          </div>
-        </div>
-      )}
+        );
+      }}
     </GraphDataSource>
   );
 }

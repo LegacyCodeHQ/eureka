8dd44fc76b24da635a0e32901b434447fe032bfa
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-17 11:39:04 +0530

    feat: get `GraphData` from a WebSocket connection

--- web-client-react/src/source/GraphDataSource.tsx
+++ web-client-react/src/source/GraphDataSource.tsx
@@ -9,11 +9,37 @@ interface GraphDataSourceProps {
 
 const GraphDataSource: React.FC<GraphDataSourceProps> = ({ children }) => {
   const [graphData, setGraphData] = useState<GraphData | null>(null);
+  const wsUrl = 'ws://localhost:7070/structure-updates';
+  let webSocket: WebSocket | null = null;
 
   useEffect(() => {
     if (process.env.NODE_ENV === 'development') {
       setGraphData(parseGraphData(graphDataJson));
+    } else {
+      const ws = new WebSocket(wsUrl);
+      webSocket = ws;
+
+      ws.onopen = () => {
+        console.log('Connected to WebSocket server');
+      };
+
+      ws.onmessage = (event) => {
+        console.log(event.data);
+        setGraphData(parseGraphData(event.data));
+      };
+
+      ws.onerror = (error) => {
+        console.log(`WebSocket error: ${error}`);
+      };
+
+      ws.onclose = () => {
+        console.log('WebSocket connection closed');
+      };
     }
+
+    return () => {
+      webSocket?.close();
+    };
   }, []);
 
   return children(graphData) || null;

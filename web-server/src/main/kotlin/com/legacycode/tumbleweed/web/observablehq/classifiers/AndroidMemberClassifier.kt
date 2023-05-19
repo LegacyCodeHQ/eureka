3794118298ff28c6205936be9ef766f2b63835b7
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-19 16:00:37 +0530

    feat: add ability to group Android framework methods

--- web-server/src/main/kotlin/com/legacycode/tumbleweed/web/observablehq/classifiers/AndroidMemberClassifier.kt
+++ web-server/src/main/kotlin/com/legacycode/tumbleweed/web/observablehq/classifiers/AndroidMemberClassifier.kt
@@ -3,8 +3,14 @@ package com.legacycode.tumbleweed.web.observablehq.classifiers
 import com.legacycode.tumbleweed.Field
 import com.legacycode.tumbleweed.FieldSignature
 import com.legacycode.tumbleweed.Member
+import com.legacycode.tumbleweed.Method
 
 class AndroidMemberClassifier : MemberClassifier {
+  companion object {
+    private const val ANDROID_PROPERTY_GROUP = 3
+    private const val ANDROID_FRAMEWORK_METHOD_GROUP = 4
+  }
+
   private val memberClassifier = BasicMemberClassifier()
 
   private val androidPackagePrefixes = listOf(
@@ -12,13 +18,36 @@ class AndroidMemberClassifier : MemberClassifier {
     "androidx",
   ).map { "$it." }
 
+  private val frameworkMethodSignatures = listOf(
+    "void onResume()",
+    "void onPause()",
+  )
+
   override fun groupOf(member: Member): Int {
-    if (member is Field) {
-      val packageName = (member.signature as FieldSignature).type.packageName
-      if (androidPackagePrefixes.any { packageName.startsWith(it) }) {
-        return 3
-      }
+    return when (member) {
+      is Field -> groupField(member)
+      is Method -> groupMethod(member)
+      else -> memberClassifier.groupOf(member)
     }
-    return memberClassifier.groupOf(member)
+  }
+
+  private fun groupField(field: Field): Int {
+    val packageName = (field.signature as FieldSignature).type.packageName
+    val isAndroidFrameworkClass = androidPackagePrefixes.any { packageName.startsWith(it) }
+    return if (isAndroidFrameworkClass) {
+      ANDROID_PROPERTY_GROUP
+    } else {
+      memberClassifier.groupOf(field)
+    }
+  }
+
+  private fun groupMethod(method: Method): Int {
+    val methodSignature = method.signature.verbose
+    val isAndroidFrameworkMethod = methodSignature in frameworkMethodSignatures
+    return if (isAndroidFrameworkMethod) {
+      ANDROID_FRAMEWORK_METHOD_GROUP
+    } else {
+      memberClassifier.groupOf(method)
+    }
   }
 }

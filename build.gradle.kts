09308f3ebb8bd860805bec31558c7dcc0119689e
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-19 07:34:11 +0530

    build: bump gson

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ subprojects {
     testImplementation("com.google.truth:truth:1.1.3")
 
     testImplementation("com.approvaltests:approvaltests:18.6.0")
-    testImplementation("com.google.code.gson:gson:2.10") /* Used by Approvals for pretty-printing JSON */
+    testImplementation("com.google.code.gson:gson:2.10.1") /* Used by Approvals for pretty-printing JSON */
   }
 
   tasks.withType<KotlinCompile> {

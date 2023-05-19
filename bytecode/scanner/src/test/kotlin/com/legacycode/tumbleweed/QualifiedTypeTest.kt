f5746c39b692cc5cfb20a591ebb38c09e4a6bec2
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-19 13:06:39 +0530

    refactor: group tests under `SimpleName`

--- bytecode/scanner/src/test/kotlin/com/legacycode/tumbleweed/QualifiedTypeTest.kt
+++ bytecode/scanner/src/test/kotlin/com/legacycode/tumbleweed/QualifiedTypeTest.kt
@@ -1,57 +1,61 @@
 package com.legacycode.tumbleweed
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class QualifiedTypeTest {
-  @ParameterizedTest
-  @ValueSource(
-    strings = [
-      "int",
-      "long",
-      "short",
-      "byte",
-      "boolean",
-      "char",
-      "float",
-      "double",
-      "void",
-    ],
-  )
-  fun `return the same type for primitive types`(type: String) {
-    assertThat(QualifiedType(type).simpleName)
-      .isEqualTo(type)
-  }
+  @Nested
+  inner class SimpleName {
+    @ParameterizedTest
+    @ValueSource(
+      strings = [
+        "int",
+        "long",
+        "short",
+        "byte",
+        "boolean",
+        "char",
+        "float",
+        "double",
+        "void",
+      ],
+    )
+    fun `return the same type for primitive types`(type: String) {
+      assertThat(QualifiedType(type).simpleName)
+        .isEqualTo(type)
+    }
 
-  @Test
-  fun `return simple names for non-primitive types`() {
-    assertThat(QualifiedType("java.lang.String").simpleName)
-      .isEqualTo("String")
-  }
+    @Test
+    fun `return simple names for non-primitive types`() {
+      assertThat(QualifiedType("java.lang.String").simpleName)
+        .isEqualTo("String")
+    }
 
-  @Test
-  fun `return simple names for primitive arrays`() {
-    assertThat(QualifiedType("[]int").simpleName)
-      .isEqualTo("[]int")
-  }
+    @Test
+    fun `return simple names for primitive arrays`() {
+      assertThat(QualifiedType("[]int").simpleName)
+        .isEqualTo("[]int")
+    }
 
-  @Test
-  fun `return simple names for non-primitive arrays`() {
-    assertThat(QualifiedType("[]java.lang.String").simpleName)
-      .isEqualTo("[]String")
-  }
+    @Test
+    fun `return simple names for non-primitive arrays`() {
+      assertThat(QualifiedType("[]java.lang.String").simpleName)
+        .isEqualTo("[]String")
+    }
 
-  @Test
-  fun `simple names for multi-dimensional primitive arrays`() {
-    assertThat(QualifiedType("[][]double").simpleName)
-      .isEqualTo("[][]double")
-  }
+    @Test
+    fun `simple names for multi-dimensional primitive arrays`() {
+      assertThat(QualifiedType("[][]double").simpleName)
+        .isEqualTo("[][]double")
+    }
 
-  @Test
-  fun `simple names for multi-dimensional non-primitive arrays`() {
-    assertThat(QualifiedType("[][]java.lang.String").simpleName)
-      .isEqualTo("[][]String")
+    @Test
+    fun `simple names for multi-dimensional non-primitive arrays`() {
+      assertThat(QualifiedType("[][]java.lang.String").simpleName)
+        .isEqualTo("[][]String")
+    }
   }
 }

d18f819b5ed7c14a88ef2196ff3d0fa94b2cf5ab
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-19 11:51:50 +0530

    refactor: introduce a `Transformer` type

--- web-server/src/main/kotlin/com/legacycode/tumbleweed/web/observablehq/ClassStructureJson.kt
+++ web-server/src/main/kotlin/com/legacycode/tumbleweed/web/observablehq/ClassStructureJson.kt
@@ -1,47 +1,6 @@
 package com.legacycode.tumbleweed.web.observablehq
 
 import com.legacycode.tumbleweed.ClassStructure
-import com.legacycode.tumbleweed.Field
-import com.legacycode.tumbleweed.Member
-import com.legacycode.tumbleweed.Method
-import com.legacycode.tumbleweed.Relationship
 
 val ClassStructure.graph: BilevelEdgeBundlingGraph
-  get() {
-    val nodes = (fields + methods).map(Member::toNode)
-    val links = relationships.map(Relationship::toLink)
-    return BilevelEdgeBundlingGraph(nodes, links, classInfoMap())
-  }
-
-private fun ClassStructure.classInfoMap(): Map<String, Map<String, Any>> {
-  val classInfo = mutableMapOf<String, Any>(
-    "name" to type.name.replace("/", "."),
-  ).apply {
-    if (superType.name != "java.lang.Object") {
-      put("extends", superType.name.replace("/", "."))
-    }
-    if (interfaces.isNotEmpty()) {
-      put("implements", interfaces.map { it.name.replace("/", ".") })
-    }
-  }
-  return mapOf("classInfo" to classInfo.toMap())
-}
-
-private fun Member.toNode(): BilevelEdgeBundlingGraph.Node {
-  val group = when (this) {
-    is Field -> 1
-    is Method -> 2
-  }
-
-  return BilevelEdgeBundlingGraph.Node(
-    id = signature.concise,
-    group = group
-  )
-}
-
-private fun Relationship.toLink(): BilevelEdgeBundlingGraph.Link {
-  return BilevelEdgeBundlingGraph.Link(
-    source = source.signature.concise,
-    target = target.signature.concise
-  )
-}
+  get() = Transformer().transform(this)
---
+++ web-server/src/main/kotlin/com/legacycode/tumbleweed/web/observablehq/Transformer.kt
@@ -0,0 +1,48 @@
+package com.legacycode.tumbleweed.web.observablehq
+
+import com.legacycode.tumbleweed.ClassStructure
+import com.legacycode.tumbleweed.Field
+import com.legacycode.tumbleweed.Member
+import com.legacycode.tumbleweed.Method
+import com.legacycode.tumbleweed.Relationship
+
+class Transformer {
+  fun transform(classStructure: ClassStructure): BilevelEdgeBundlingGraph {
+    val nodes = (classStructure.fields + classStructure.methods).map(::toNode)
+    val links = classStructure.relationships.map(::toLink)
+    return BilevelEdgeBundlingGraph(nodes, links, classStructure.classInfoMap())
+  }
+
+  private fun toNode(member: Member): BilevelEdgeBundlingGraph.Node {
+    val group = when (member) {
+      is Field -> 1
+      is Method -> 2
+    }
+
+    return BilevelEdgeBundlingGraph.Node(
+      id = member.signature.concise,
+      group = group
+    )
+  }
+
+  private fun toLink(relationship: Relationship): BilevelEdgeBundlingGraph.Link {
+    return BilevelEdgeBundlingGraph.Link(
+      source = relationship.source.signature.concise,
+      target = relationship.target.signature.concise
+    )
+  }
+
+  private fun ClassStructure.classInfoMap(): Map<String, Map<String, Any>> {
+    val classInfo = mutableMapOf<String, Any>(
+      "name" to type.name.replace("/", "."),
+    ).apply {
+      if (superType.name != "java.lang.Object") {
+        put("extends", superType.name.replace("/", "."))
+      }
+      if (interfaces.isNotEmpty()) {
+        put("implements", interfaces.map { it.name.replace("/", ".") })
+      }
+    }
+    return mapOf("classInfo" to classInfo.toMap())
+  }
+}

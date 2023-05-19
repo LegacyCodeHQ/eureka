1255ce870e18bbfb51c08ab8af34666b8f87c094
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-19 22:39:05 +0530

    feat: add Android mode logo to toolbar

--- web-client-react/src/toolbar/Toolbar.tsx
+++ web-client-react/src/toolbar/Toolbar.tsx
@@ -4,6 +4,7 @@ import SimpleJvmClassName from './SimpleJvmClassName';
 import AppVersion from './AppVersion';
 import './Toolbar.css';
 import GitHubProjectLogo from './GitHubProjectLogo';
+import AndroidLogo from './AndroidLogo';
 
 interface ToolbarProps {
   data: GraphData | null;
@@ -13,6 +14,7 @@ const Toolbar: React.FC<ToolbarProps> = ({ data }) => {
   return (
     <div className="toolbar">
       <span className="product-name">TWD</span>
+      <AndroidLogo />
       {data && <SimpleJvmClassName classInfo={data.meta.classInfo} />}
       <AppVersion />
       <GitHubProjectLogo account="LegacyCodeHQ" project="tumbleweed" />

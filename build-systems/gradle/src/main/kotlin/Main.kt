293c40775bb0c1c9a7b6de5fb4d69cb3c610168a
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-25 22:21:05 +0530

    feat: execute dependencies task command

--- build-systems/gradle/src/main/kotlin/Main.kt
+++ build-systems/gradle/src/main/kotlin/Main.kt
@@ -4,13 +4,15 @@ import com.legacycode.ureka.gradle.commands.GradleProjectsCommand
 import java.io.File
 
 fun main() {
-  val projectsCommand = GradleProjectsCommand(File("").absoluteFile)
+  val projectRoot = File("").absoluteFile
+  val cmdFile = projectRoot.resolve("gradlew")
+  val projectsCommand = GradleProjectsCommand(projectRoot)
   val projectsOutput = projectsCommand.execute()
 
   val projectStructure = ProjectStructure.from(projectsOutput)
   val gradleDependenciesCommands = GradleDependenciesCommand.from(projectStructure)
 
   gradleDependenciesCommands.onEach {
-    println(it.text)
+    println(it.execute(cmdFile))
   }
 }
--- build-systems/gradle/src/main/kotlin/com/legacycode/ureka/gradle/commands/Command.kt
+++ build-systems/gradle/src/main/kotlin/com/legacycode/ureka/gradle/commands/Command.kt
@@ -1,13 +1,14 @@
 package com.legacycode.ureka.gradle.commands
 
+import java.io.File
 import org.buildobjects.process.ProcBuilder
 
 interface Command {
   val text: String
 
-  fun execute(): String {
+  fun execute(cmdFile: File? = null): String {
     val cmdArgs = text.split(" ")
-    val cmd = cmdArgs.first()
+    val cmd = cmdFile?.path ?: cmdArgs.first()
     val args = cmdArgs.drop(1).toTypedArray()
     return ProcBuilder.run(cmd, *args)!!
   }

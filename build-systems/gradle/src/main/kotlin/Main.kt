4ffbb4709cfcf84b703f70ea8954fb477565f37f
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-26 09:04:26 +0530

    refactor: extract function

--- build-systems/gradle/src/main/kotlin/Main.kt
+++ build-systems/gradle/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import com.legacycode.ureka.gradle.CommandOutput
+import com.legacycode.ureka.gradle.Project
 import com.legacycode.ureka.gradle.ProjectStructure
 import com.legacycode.ureka.gradle.SubprojectDependency
 import com.legacycode.ureka.gradle.commands.GradleDependenciesCommand
@@ -14,13 +15,28 @@ fun main() {
   val projectStructure = ProjectStructure.from(CommandOutput(projectsOutput))
   val gradleDependenciesCommands = GradleDependenciesCommand.from(projectStructure)
 
-  gradleDependenciesCommands.onEach { dependenciesCommand ->
-    println(dependenciesCommand.project.name)
-    val output = CommandOutput(dependenciesCommand.execute(cmdFile))
-    val dependencies = SubprojectDependency.from(output)
+  val resolvedDependencies = resolveDependencies(cmdFile, gradleDependenciesCommands)
+
+  resolvedDependencies.entries.onEach { (project, dependencies) ->
+    println("${project.name} (${dependencies.size})")
+
     dependencies.forEach {
       println("  → ${it.name}")
     }
     println()
   }
+}
+
+private fun resolveDependencies(
+  cmdFile: File,
+  commands: List<GradleDependenciesCommand>,
+): Map<Project, List<SubprojectDependency>> {
+  val resolvedDependencies = mutableMapOf<Project, List<SubprojectDependency>>()
+  commands.onEach { dependenciesCommand ->
+    val project = dependenciesCommand.project
+    val output = CommandOutput(dependenciesCommand.execute(cmdFile))
+    val dependencies = SubprojectDependency.from(output)
+    resolvedDependencies[project] = dependencies
+  }
+  return resolvedDependencies.toMap()
 }

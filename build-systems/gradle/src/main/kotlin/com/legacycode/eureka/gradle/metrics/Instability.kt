7440ca896bc7d561f6a9181e84e18ef4f34dd4b1
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-30 18:04:19 +0530

    refactor: replace hard-coded values with constants

--- build-systems/gradle/src/main/kotlin/com/legacycode/eureka/gradle/metrics/Instability.kt
+++ build-systems/gradle/src/main/kotlin/com/legacycode/eureka/gradle/metrics/Instability.kt
@@ -1,28 +1,36 @@
 package com.legacycode.eureka.gradle.metrics
 
 import java.math.BigDecimal
-import java.math.RoundingMode
+import java.math.RoundingMode.HALF_UP
 
 data class Instability(
   val outgoing: Int,
   val incoming: Int,
 ) {
+  companion object {
+    const val MAXIMALLY_STABLE = "0"
+    const val MAXIMALLY_UNSTABLE = "1"
+    const val UNUSED = "∞"
+
+    private const val DECIMAL_PLACES_TO_SHOW = 2
+  }
+
   val value: String
     get() {
       if (outgoing == 0 && incoming == 0) {
-        return "∞"
+        return UNUSED
       }
 
       return when (val i = outgoing.toDouble() / (incoming + outgoing)) {
-        1.0 -> "1"
-        0.0 -> "0"
+        1.0 -> MAXIMALLY_UNSTABLE
+        0.0 -> MAXIMALLY_STABLE
         else -> twoDecimalPoints(i)
       }
     }
 
   private fun twoDecimalPoints(value: Double): String {
     return BigDecimal(value)
-      .setScale(2, RoundingMode.HALF_UP)
+      .setScale(DECIMAL_PLACES_TO_SHOW, HALF_UP)
       .toString()
   }
 }

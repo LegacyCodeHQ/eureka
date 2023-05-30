97c2e0a7aebfa11eb95bce98e96cab84bc1f539b
Ragunath Jawahar <ragunath@redgreen.io>
2023-05-30 20:00:23 +0530

    feat: print link to a online Graphviz editor tool

--- cli/src/main/kotlin/com/legacycode/tumbleweed/cli/modules/ModulesCommand.kt
+++ cli/src/main/kotlin/com/legacycode/tumbleweed/cli/modules/ModulesCommand.kt
@@ -22,14 +22,16 @@ class ModulesCommand : Runnable {
     if (!projectRoot.isGradleProject()) {
       println("Ouch... does the path contain a Gradle project? ($projectRoot)")
     } else {
-      var uml: String
+      var graphviz: String
       val timeMillis = measureTimeMillis {
-        uml = runGradleCommands(projectRoot)
+        graphviz = runGradleCommands(projectRoot)
       }
       println("Execution took: ${timeMillis}ms")
       println()
-      println(uml)
+      println(graphviz)
     }
+
+    println("Copy and paste the output at https://dreampuf.github.io/GraphvizOnline")
   }
 
   private fun File.isGradleProject(): Boolean =

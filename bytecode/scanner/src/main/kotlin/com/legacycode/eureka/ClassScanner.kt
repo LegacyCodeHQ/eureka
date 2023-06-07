4afa80c9de4f2ed2beda96c5f95b131a625be99f
Ragunath Jawahar <ragunath@redgreen.io>
2023-06-07 21:17:47 +0530

    refactor: inline class

--- bytecode/scanner/src/main/kotlin/com/legacycode/eureka/ClassScanner.kt
+++ bytecode/scanner/src/main/kotlin/com/legacycode/eureka/ClassScanner.kt
@@ -1,19 +1,27 @@
 package com.legacycode.eureka
 
+import com.legacycode.eureka.Opcodes.areturn
+import com.legacycode.eureka.Opcodes.iconst_0
+import com.legacycode.eureka.Opcodes.iconst_m1
+import com.legacycode.eureka.Opcodes.invokespecial
 import java.io.BufferedInputStream
 import java.io.File
 import net.bytebuddy.jar.asm.ClassReader
 import net.bytebuddy.jar.asm.ClassVisitor
 import net.bytebuddy.jar.asm.FieldVisitor
+import net.bytebuddy.jar.asm.Handle
+import net.bytebuddy.jar.asm.Label
 import net.bytebuddy.jar.asm.MethodVisitor
 import net.bytebuddy.jar.asm.Opcodes.ACC_STATIC
 import net.bytebuddy.jar.asm.Opcodes.ASM9
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+typealias ConstantPool = MutableMap<Any?, Field>
+typealias Opcode = Int
+
 const val ASM_API_VERSION = ASM9
 
-typealias ConstantPool = MutableMap<Any?, Field>
 fun constantPool(): ConstantPool = mutableMapOf()
 
 object ClassScanner {
@@ -104,7 +112,127 @@ object ClassScanner {
 
         val method = Method(name!!, MethodDescriptor(descriptor!!), topLevelType)
         outMethods.add(method)
-        return InstructionScanner.scan(topLevelType, method, constantPool, outRelationships)
+
+        return object : MethodVisitor(ASM_API_VERSION) {
+          private var maybeConstantFieldReferencedByInsn: Field? = null
+
+          override fun visitFieldInsn(
+            opcode: Opcode,
+            owner: String,
+            fieldName: String,
+            fieldDescriptor: String,
+          ) {
+            logger.debug("Visiting field instruction ({}): {}/{}", opcode.instruction, owner, fieldName)
+
+            val field = Field(fieldName, FieldDescriptor.from(fieldDescriptor), QualifiedType(owner))
+            val relationship = Relationship(method, field, Relationship.Type.from(opcode))
+            if (owner == topLevelType.name) {
+              logger.debug("Adding relationship: {}", relationship)
+              outRelationships.add(relationship)
+            } else {
+              logger.debug("Skipping relationship: {}", relationship)
+            }
+          }
+
+          override fun visitMethodInsn(
+            opcode: Opcode,
+            owner: String,
+            methodName: String,
+            methodDescriptor: String,
+            isInterface: Boolean,
+          ) {
+            logger.debug("Visiting method instruction ({}): {}/{}", opcode.instruction, owner, methodName)
+
+            if (topLevelType.name.startsWith(owner) || owner.startsWith("${topLevelType.name}\$")) {
+              val callee = Method(methodName, MethodDescriptor(methodDescriptor), QualifiedType(owner))
+              val relationship = Relationship(method, callee, Relationship.Type.from(opcode))
+
+              if (method.name == "<clinit>" && opcode == invokespecial) {
+                logger.debug("Skipping synthetic bridge call from static block: {}", relationship)
+              } else {
+                logger.debug("Adding relationship: {}", relationship)
+                outRelationships.add(relationship)
+              }
+            } else {
+              logger.debug("Skipping method instruction ({}): {}/{}", opcode.instruction, owner, methodName)
+            }
+
+            if (maybeConstantFieldReferencedByInsn != null) {
+              val relationship = Relationship(method, maybeConstantFieldReferencedByInsn!!, Relationship.Type.Reads)
+
+              logger.debug("Adding relationship: {}", relationship)
+              outRelationships.add(relationship)
+              maybeConstantFieldReferencedByInsn = null
+            }
+          }
+
+          override fun visitInvokeDynamicInsn(
+            name: String?,
+            descriptor: String?,
+            bootstrapMethodHandle: Handle?,
+            vararg bootstrapMethodArguments: Any?,
+          ) {
+            logger.debug(
+              "Visiting invoke dynamic instruction: {}, {}",
+              name,
+              bootstrapMethodArguments.contentToString()
+            )
+
+            if (bootstrapMethodArguments.size > 1) {
+              val methodHandle = bootstrapMethodArguments[1] as Handle
+              val callee = Method(methodHandle.name, MethodDescriptor(methodHandle.desc), topLevelType)
+              outRelationships.add(Relationship(method, callee, Relationship.Type.Calls))
+            } else {
+              logger.debug("Ignoring dynamic instruction: {}{}", name, descriptor)
+            }
+            super.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, *bootstrapMethodArguments)
+          }
+
+          override fun visitLdcInsn(value: Any?) {
+            logger.debug("Visiting ldc instruction: {}", value)
+
+            maybeConstantFieldReferencedByInsn = constantPool[value]
+            super.visitLdcInsn(value)
+          }
+
+          override fun visitIntInsn(opcode: Opcode, operand: Int) {
+            logger.debug("Visiting int instruction: {}", opcode.instruction)
+
+            maybeConstantFieldReferencedByInsn = constantPool[operand]
+            super.visitIntInsn(opcode, operand)
+          }
+
+          override fun visitInsn(opcode: Opcode) {
+            logger.debug("Visiting instruction: {}", opcode.instruction)
+
+            if (opcode == iconst_m1) {
+              maybeConstantFieldReferencedByInsn = constantPool[-1]
+            } else if (Opcodes.isIntInsn(opcode)) {
+              maybeConstantFieldReferencedByInsn = constantPool[opcode - iconst_0]
+            }
+
+            if (opcode == areturn && maybeConstantFieldReferencedByInsn != null) {
+              val relationship = Relationship(method, maybeConstantFieldReferencedByInsn!!, Relationship.Type.Reads)
+              logger.debug("Adding relationship: {}", relationship)
+              outRelationships.add(relationship)
+              maybeConstantFieldReferencedByInsn = null
+            }
+            super.visitInsn(opcode)
+          }
+
+          override fun visitJumpInsn(opcode: Opcode, label: Label?) {
+            logger.debug("Visiting jump instruction: {}", opcode.instruction)
+
+            if (maybeConstantFieldReferencedByInsn != null) {
+              val relationship = Relationship(method, maybeConstantFieldReferencedByInsn!!, Relationship.Type.Reads)
+
+              logger.debug("Adding relationship: {}", relationship)
+              outRelationships.add(relationship)
+              maybeConstantFieldReferencedByInsn = null
+            }
+            super.visitJumpInsn(opcode, label)
+          }
+        }
       }
 
       override fun visitInnerClass(name: String, outerName: String?, innerName: String?, access: Int) {
@@ -133,3 +261,30 @@ object ClassScanner {
     )
   }
 }
+
+@Suppress("MagicNumber")
+internal val Opcode.instruction: String
+  get() {
+    return when (this) {
+      0xb1 -> "return"
+      0xb2 -> "getstatic"
+      0xb3 -> "putstatic"
+      0xb5 -> "putfield"
+      0xb4 -> "getfield"
+      invokespecial -> "invokespecial"
+      0xb6 -> "invokevirtual"
+      0xb8 -> "invokestatic"
+      0xb9 -> "invokeinterface"
+      0x10 -> "bipush"
+      iconst_m1 -> "iconst_m1"
+      iconst_0 -> "iconst_0"
+      Opcodes.iconst_1 -> "iconst_1"
+      Opcodes.iconst_2 -> "iconst_2"
+      Opcodes.iconst_3 -> "iconst_3"
+      Opcodes.iconst_4 -> "iconst_4"
+      Opcodes.iconst_5 -> "iconst_5"
+      0xa0 -> "if_icmpne"
+      0xb0 -> "areturn"
+      else -> "unmapped (${"0x%02x".format(this)})})"
+    }
+  }

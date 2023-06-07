96432a3f4187d808c10e57ee6831610fc3142544
Ragunath Jawahar <ragunath@redgreen.io>
2023-06-07 15:00:40 +0530

    test: use the eureka package name

--- bytecode/scanner/src/test/kotlin/com/legacycode/eureka/FieldSignatureTest.kt
+++ bytecode/scanner/src/test/kotlin/com/legacycode/eureka/FieldSignatureTest.kt
@@ -7,7 +7,7 @@ internal class FieldSignatureTest {
   @Test
   internal fun `concise discards the outer class names`() {
     // given
-    val fieldSignature = FieldSignature("apiKey", QualifiedType("io.redgreen.twd.Secure\$ApiKey"))
+    val fieldSignature = FieldSignature("apiKey", QualifiedType("com.legacycode.eureka.Secure\$ApiKey"))
 
     // when & then
     assertThat(fieldSignature.concise)

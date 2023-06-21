1e098d271e8054ff5faf978e93099fdbfb9ddada
Ragunath Jawahar <ragunath@redgreen.io>
2023-06-21 12:34:54 +0530

    refactor: rename parameters and variables

--- web-client-react/src/viz/model/Cluster.ts
+++ web-client-react/src/viz/model/Cluster.ts
@@ -5,31 +5,32 @@ class Cluster {
     // empty
   }
 
-  static from(classLinks: Link[], startNode: string, stopNode: string | null = null): Cluster {
+  static from(classLinks: Link[], startNodeId: string, stopNodeId: string | null = null): Cluster {
     const visited = new Set<string>();
     const connectedLinks = new Set<Link>();
-    const stack: string[] = [startNode];
+    const stack: string[] = [startNodeId];
 
     while (stack.length > 0) {
-      const currentNode = stack.pop()!;
-      visited.add(currentNode);
+      const currentNodeId = stack.pop()!;
+      visited.add(currentNodeId);
 
-      const relatedLinks = classLinks.filter((link) => link.source === currentNode || link.target === currentNode);
+      const relatedLinks = classLinks.filter((link) => link.source === currentNodeId || link.target === currentNodeId);
 
       relatedLinks.forEach((link) => connectedLinks.add(link));
 
       for (const link of relatedLinks) {
-        const nextNode = link.source === currentNode ? link.target : link.source;
-        if (!visited.has(nextNode) && !stack.includes(nextNode)) {
-          stack.push(nextNode);
+        const nextNodeId = link.source === currentNodeId ? link.target : link.source;
+        if (!visited.has(nextNodeId) && !stack.includes(nextNodeId)) {
+          stack.push(nextNodeId);
         }
       }
     }
 
-    if (stopNode) {
+    if (stopNodeId) {
       const filteredLinks = Array.from(connectedLinks).filter(
         (link) =>
-          link.source !== stopNode || (link.source === stopNode && pathExists(link.target, startNode, classLinks)),
+          link.source !== stopNodeId ||
+          (link.source === stopNodeId && pathExists(link.target, startNodeId, classLinks)),
       );
       return new Cluster(filteredLinks);
     }
@@ -38,23 +39,23 @@ class Cluster {
   }
 }
 
-function pathExists(startNode: string, destinationNode: string, classLinks: Link[]) {
+function pathExists(startNodeId: string, destinationNodeId: string, classLinks: Link[]) {
   const visited = new Set();
-  const stack = [startNode];
+  const stack = [startNodeId];
 
   while (stack.length > 0) {
-    const currentNode = stack.pop();
-    visited.add(currentNode);
+    const currentNodeId = stack.pop();
+    visited.add(currentNodeId);
 
-    const relatedLinks = classLinks.filter((link) => link.source === currentNode || link.target === currentNode);
+    const relatedLinks = classLinks.filter((link) => link.source === currentNodeId || link.target === currentNodeId);
 
     for (const link of relatedLinks) {
-      const nextNode = link.source === currentNode ? link.target : link.source;
-      if (nextNode === destinationNode) {
+      const nextNodeId = link.source === currentNodeId ? link.target : link.source;
+      if (nextNodeId === destinationNodeId) {
         return true;
       }
-      if (!visited.has(nextNode) && !stack.includes(nextNode)) {
-        stack.push(nextNode);
+      if (!visited.has(nextNodeId) && !stack.includes(nextNodeId)) {
+        stack.push(nextNodeId);
       }
     }
   }

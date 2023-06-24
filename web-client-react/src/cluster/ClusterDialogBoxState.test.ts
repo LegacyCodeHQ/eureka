628dfb610a255e5107cd23e2b01da8fbb89ad924
Ragunath Jawahar <ragunath@redgreen.io>
2023-06-24 22:46:24 +0530

    refactor: group tests based on functionality

--- web-client-react/src/cluster/ClusterDialogBoxState.test.ts
+++ web-client-react/src/cluster/ClusterDialogBoxState.test.ts
@@ -19,61 +19,68 @@ describe('Cluster dialog box state', () => {
     expect(initialState).toMatchSnapshot();
   });
 
-  it('should not filter members when the search term is empty', () => {
-    // when
-    const searchState = initialState.search('');
+  describe('search functionality', () => {
+    it('should not filter members when the search term is empty', () => {
+      // when
+      const searchState = initialState.search('');
 
-    // then
-    expect(searchState).toMatchSnapshot();
+      // then
+      expect(searchState).toMatchSnapshot();
+    });
+
+    it('should not filter members when the search term only contains spaces', () => {
+      // when
+      const searchState = initialState.search('      ');
+
+      // then
+      expect(searchState).toMatchSnapshot();
+      expect(searchState.isSearchTermEmpty()).toBe(true);
+    });
+
+    it('should not filter members when the search term has < 2 characters', () => {
+      // when
+      const searchState = initialState.search('c');
+
+      // then
+      expect(searchState).toMatchSnapshot();
+    });
+
+    it('should filter members based on the search term', () => {
+      // when
+      const searchResultState = initialState.search('chr');
+
+      // then
+      expect(searchResultState).toMatchSnapshot();
+    });
+
+    it('should filter members based on the search term but ignore whitespaces', () => {
+      // when
+      const searchResultState = initialState.search('  chr  ');
+
+      // then
+      expect(searchResultState).toMatchSnapshot();
+    });
+
+    it('should not have filtered results for a non-matching search term', () => {
+      // when
+      const searchResultState = initialState.search('hello');
+
+      // then
+      expect(searchResultState).toMatchSnapshot();
+      expect(searchResultState.isSearchTermEmpty()).toBe(false);
+    });
   });
 
-  it('should not filter members when the search term only contains spaces', () => {
-    // when
-    const searchState = initialState.search('      ');
+  describe('focused member functionality', () => {
+    let searchResultState: ClusterDialogBoxState;
 
-    // then
-    expect(searchState).toMatchSnapshot();
-    expect(searchState.isSearchTermEmpty()).toBe(true);
-  });
+    beforeEach(() => {
+      searchResultState = initialState.search('void');
+    });
 
-  it('should not filter members when the search term has < 2 characters', () => {
-    // when
-    const searchState = initialState.search('c');
-
-    // then
-    expect(searchState).toMatchSnapshot();
-  });
-
-  it('should filter members based on the search term', () => {
-    // when
-    const searchResultState = initialState.search('chr');
-
-    // then
-    expect(searchResultState).toMatchSnapshot();
-  });
-
-  it('should filter members based on the search term but ignore whitespaces', () => {
-    // when
-    const searchResultState = initialState.search('  chr  ');
-
-    // then
-    expect(searchResultState).toMatchSnapshot();
-  });
-
-  it('should not have filtered results for a non-matching search term', () => {
-    // when
-    const searchResultState = initialState.search('hello');
-
-    // then
-    expect(searchResultState).toMatchSnapshot();
-    expect(searchResultState.isSearchTermEmpty()).toBe(false);
-  });
-
-  it('should select the first matching member and mark it as focused from the search result', () => {
-    // when
-    const searchResultState = initialState.search('void');
-
-    // then
-    expect(searchResultState).toMatchSnapshot();
+    it('should select the first matching member and mark it as focused from the search result', () => {
+      expect(searchResultState).toMatchSnapshot();
+      expect(searchResultState.focusedMember).toEqual(new Member('void onCreate()'));
+    });
   });
 });

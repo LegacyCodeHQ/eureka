d1c7c39921afc8bbbffb946cfb1783290de860ef
Ragunath Jawahar <ragunath@redgreen.io>
2023-06-26 13:48:27 +0530

    refactor: change signature

--- web-client-react/src/cluster/ClusterDialogBoxState.ts
+++ web-client-react/src/cluster/ClusterDialogBoxState.ts
@@ -1,16 +1,16 @@
 import { Member } from './Member';
 
 class SelectionModel {
-  private constructor(
+  constructor(
     public readonly searchTerm: string,
-    public readonly focused: Member | null,
     public readonly searchResult: Member[],
+    public readonly focused: Member | null,
     public readonly selected: Member | null,
   ) {
     // empty
   }
 
-  static DEFAULT = new SelectionModel('', null, [], null);
+  static DEFAULT = new SelectionModel('', [], null, null);
 
   isSearchTermEmpty(): boolean {
     return this.sanitizeSearchTerm(this.searchTerm).length === 0;
@@ -26,7 +26,7 @@ class SelectionModel {
       currentFocusedMemberIndex + 1 === this.searchResult.length ? 0 : currentFocusedMemberIndex + 1;
     const nextFocusedMember = this.searchResult[nextFocusedMemberIndex];
 
-    return new SelectionModel(this.searchTerm, nextFocusedMember, this.searchResult, this.selected);
+    return new SelectionModel(this.searchTerm, this.searchResult, nextFocusedMember, this.selected);
   }
 
   focusPreviousMember(): SelectionModel {
@@ -35,7 +35,7 @@ class SelectionModel {
       currentFocusedMemberIndex - 1 < 0 ? this.searchResult.length - 1 : currentFocusedMemberIndex - 1;
     const previousFocusedMember = this.searchResult[previousFocusedMemberIndex];
 
-    return new SelectionModel(this.searchTerm, previousFocusedMember, this.searchResult, this.selected);
+    return new SelectionModel(this.searchTerm, this.searchResult, previousFocusedMember, this.selected);
   }
 
   search(searchTerm: string, members: Member[]): SelectionModel {
@@ -54,15 +54,15 @@ class SelectionModel {
     if (trimmedSearchTerm.length > 0 && filteredMembers.length > 0) {
       focusedMember = filteredMembers[0];
     }
-    return new SelectionModel(trimmedSearchTerm, focusedMember, filteredMembers, this.selected);
+    return new SelectionModel(trimmedSearchTerm, filteredMembers, focusedMember, this.selected);
   }
 
   select(): SelectionModel {
-    return new SelectionModel(this.searchTerm, this.focused, this.searchResult, this.focused);
+    return new SelectionModel(this.searchTerm, this.searchResult, this.focused, this.focused);
   }
 
   deselect(): SelectionModel {
-    return new SelectionModel(this.searchTerm, this.focused, this.searchResult, null);
+    return new SelectionModel(this.searchTerm, this.searchResult, this.focused, null);
   }
 }
 

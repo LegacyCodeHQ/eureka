be1218f39bc2c2d26f6e309af22deb2658f4c8d8
Ragunath Jawahar <ragunath@redgreen.io>
2023-07-03 12:19:20 +0530

    build: prepare release

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -3,7 +3,7 @@ import java.util.Properties
 import org.jreleaser.model.Active
 
 @Suppress("PropertyName")
-val CLI_VERSION = "0.68.0-SNAPSHOT"
+val CLI_VERSION = "0.68.0"
 
 plugins {
   application

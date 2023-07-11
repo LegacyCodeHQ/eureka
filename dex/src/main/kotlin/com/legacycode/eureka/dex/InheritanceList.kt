faf4081b59d3539e43b3d7235940aeeda263a6b6
Ragunath Jawahar <ragunath@redgreen.io>
2023-07-11 09:39:47 +0530

    feat: query children

--- dex/src/main/kotlin/com/legacycode/eureka/dex/InheritanceList.kt
+++ dex/src/main/kotlin/com/legacycode/eureka/dex/InheritanceList.kt
@@ -2,4 +2,23 @@ package com.legacycode.eureka.dex
 
 class InheritanceList {
   val isEmpty: Boolean get() = true
+
+  private val parentChildrenMap: MutableMap<String, MutableSet<String>> = mutableMapOf()
+
+  fun add(child: String, ancestor: String) {
+    var children = parentChildrenMap[ancestor]
+    if (children == null) {
+      children = mutableSetOf()
+      parentChildrenMap[ancestor] = children
+    }
+    children.add(child)
+  }
+
+  fun ancestors(): Set<String> {
+    return parentChildrenMap.keys.toSet()
+  }
+
+  fun children(ancestor: String): Set<String> {
+    return parentChildrenMap[ancestor]!!
+  }
 }
--- dex/src/test/kotlin/com/legacycode/eureka/dex/InheritanceListTest.kt
+++ dex/src/test/kotlin/com/legacycode/eureka/dex/InheritanceListTest.kt
@@ -4,13 +4,38 @@ import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
 
 class InheritanceListTest {
+  private val inheritanceList = InheritanceList()
+
   @Test
   fun `it can create an empty inheritance list`() {
-    // given
-    val inheritanceList = InheritanceList()
-
     // when & then
     assertThat(inheritanceList.isEmpty)
       .isTrue()
+  }
+
+  @Test
+  fun `it can get the ancestor`() {
+    // when
+    inheritanceList.add("Lcom/legacycode/dex/Child", "Ljava/lang/Object;")
+
+    // then
+    assertThat(inheritanceList.ancestors())
+      .containsExactly("Ljava/lang/Object;")
+  }
+
+  @Test
+  fun `it can get the children`() {
+    // when
+    with(inheritanceList) {
+      add("Lcom/legacycode/dex/Child", "Ljava/lang/Object;")
+      add("Lcom/legacycode/dex/Node", "Ljava/lang/Object;")
+    }
+
+    // then
+    assertThat(inheritanceList.children("Ljava/lang/Object;"))
+      .containsExactly(
+        "Lcom/legacycode/dex/Child",
+        "Lcom/legacycode/dex/Node",
+      )
   }
 }

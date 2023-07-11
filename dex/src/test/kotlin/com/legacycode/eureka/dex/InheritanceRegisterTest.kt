7212a3ba19fbfb23234f2d4539e1a9616017c8c8
Ragunath Jawahar <ragunath@redgreen.io>
2023-07-11 11:41:14 +0530

    test: update test data

--- dex/src/test/kotlin/com/legacycode/eureka/dex/InheritanceRegisterTest.kt
+++ dex/src/test/kotlin/com/legacycode/eureka/dex/InheritanceRegisterTest.kt
@@ -16,7 +16,7 @@ class InheritanceRegisterTest {
   @Test
   fun `a registry with at least one entry is non-empty`() {
     // when
-    register.add(Ancestor("Ljava/lang/Object;"), Child("Lcom/legacycode/dex/Child"))
+    register.add(Ancestor("Ljava/lang/Object;"), Child("Lcom/legacycode/dex/Child;"))
 
     // then
     assertThat(register.isEmpty)
@@ -26,7 +26,7 @@ class InheritanceRegisterTest {
   @Test
   fun `get ancestor`() {
     // when
-    register.add(Ancestor("Ljava/lang/Object;"), Child("Lcom/legacycode/dex/Child"))
+    register.add(Ancestor("Ljava/lang/Object;"), Child("Lcom/legacycode/dex/Child;"))
 
     // then
     assertThat(register.ancestors())
@@ -37,15 +37,15 @@ class InheritanceRegisterTest {
   fun `get children`() {
     // when
     with(register) {
-      add(Ancestor("Ljava/lang/Object;"), Child("Lcom/legacycode/dex/Child"))
-      add(Ancestor("Ljava/lang/Object;"), Child("Lcom/legacycode/dex/Node"))
+      add(Ancestor("Ljava/lang/Object;"), Child("Lcom/legacycode/dex/Child;"))
+      add(Ancestor("Ljava/lang/Object;"), Child("Lcom/legacycode/dex/Node;"))
     }
 
     // then
     assertThat(register.children(Ancestor("Ljava/lang/Object;")))
       .containsExactly(
-        Child("Lcom/legacycode/dex/Child"),
-        Child("Lcom/legacycode/dex/Node"),
+        Child("Lcom/legacycode/dex/Child;"),
+        Child("Lcom/legacycode/dex/Node;"),
       )
   }
 
@@ -53,8 +53,8 @@ class InheritanceRegisterTest {
   fun `return an empty set of children when parent does not exist`() {
     // when
     with(register) {
-      add(Ancestor("Ljava/lang/Object;"), Child("Lcom/legacycode/dex/Child"))
-      add(Ancestor("Ljava/lang/Object;"), Child("Lcom/legacycode/dex/Node"))
+      add(Ancestor("Ljava/lang/Object;"), Child("Lcom/legacycode/dex/Child;"))
+      add(Ancestor("Ljava/lang/Object;"), Child("Lcom/legacycode/dex/Node;"))
     }
 
     // then
@@ -66,11 +66,11 @@ class InheritanceRegisterTest {
   fun `it can build a tree`() {
     // when
     with(register) {
-      add(Ancestor("Landroid/app/Activity;"), Child("Landroidx/app/AppCompatActivity"))
-      add(Ancestor("Landroidx/app/AppCompatActivity"), Child("Lcom/legacycode/app/BaseActivity"))
-      add(Ancestor("Lcom/legacycode/app/BaseActivity"), Child("Landroid/app/HomeActivity"))
-      add(Ancestor("Landroidx/app/AppCompatActivity"), Child("Lcom/legacycode/MetricsActivity"))
-      add(Ancestor("Landroid/app/Fragment"), Child("Landroidx/app/AppCompatFragment"))
+      add(Ancestor("Landroid/app/Activity;"), Child("Landroidx/app/AppCompatActivity;"))
+      add(Ancestor("Landroidx/app/AppCompatActivity;"), Child("Lcom/legacycode/app/BaseActivity;"))
+      add(Ancestor("Lcom/legacycode/app/BaseActivity;"), Child("Landroid/app/HomeActivity;"))
+      add(Ancestor("Landroidx/app/AppCompatActivity;"), Child("Lcom/legacycode/MetricsActivity;"))
+      add(Ancestor("Landroid/app/Fragment;"), Child("Landroidx/app/AppCompatFragment;"))
     }
 
     // then

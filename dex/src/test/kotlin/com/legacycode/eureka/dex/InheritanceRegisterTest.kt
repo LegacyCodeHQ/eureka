5836a22f4198a93442372f1eab777f9850924c07
Ragunath Jawahar <ragunath@redgreen.io>
2023-07-11 11:55:54 +0530

    refactor: convert tree test to a nested test

--- dex/src/test/kotlin/com/legacycode/eureka/dex/InheritanceRegisterTest.kt
+++ dex/src/test/kotlin/com/legacycode/eureka/dex/InheritanceRegisterTest.kt
@@ -2,6 +2,7 @@ package com.legacycode.eureka.dex
 
 import com.google.common.truth.Truth.assertThat
 import org.approvaltests.Approvals
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class InheritanceRegisterTest {
@@ -62,18 +63,21 @@ class InheritanceRegisterTest {
       .isEmpty()
   }
 
-  @Test
-  fun `it can build a tree`() {
-    // when
-    with(register) {
-      add(Ancestor("Landroid/app/Activity;"), Child("Landroidx/app/AppCompatActivity;"))
-      add(Ancestor("Landroidx/app/AppCompatActivity;"), Child("Lcom/legacycode/app/BaseActivity;"))
-      add(Ancestor("Lcom/legacycode/app/BaseActivity;"), Child("Landroid/app/HomeActivity;"))
-      add(Ancestor("Landroidx/app/AppCompatActivity;"), Child("Lcom/legacycode/MetricsActivity;"))
-      add(Ancestor("Landroid/app/Fragment;"), Child("Landroidx/app/AppCompatFragment;"))
-    }
+  @Nested
+  inner class Tree {
+    @Test
+    fun `it can build a tree`() {
+      // when
+      with(register) {
+        add(Ancestor("Landroid/app/Activity;"), Child("Landroidx/app/AppCompatActivity;"))
+        add(Ancestor("Landroidx/app/AppCompatActivity;"), Child("Lcom/legacycode/app/BaseActivity;"))
+        add(Ancestor("Lcom/legacycode/app/BaseActivity;"), Child("Landroid/app/HomeActivity;"))
+        add(Ancestor("Landroidx/app/AppCompatActivity;"), Child("Lcom/legacycode/MetricsActivity;"))
+        add(Ancestor("Landroid/app/Fragment;"), Child("Landroidx/app/AppCompatFragment;"))
+      }
 
-    // then
-    Approvals.verify(register.tree(Ancestor("Landroid/app/Activity;"), TestTreeBuilder()))
+      // then
+      Approvals.verify(register.tree(Ancestor("Landroid/app/Activity;"), TestTreeBuilder()))
+    }
   }
 }

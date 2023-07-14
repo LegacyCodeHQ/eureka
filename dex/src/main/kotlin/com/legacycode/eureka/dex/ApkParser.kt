e2d6374780d83c530b90677dedec744839c8a14b
Ragunath Jawahar <ragunath@redgreen.io>
2023-07-14 08:52:02 +0530

    feat(android): ignore anonymous inner classes

--- dex/src/main/kotlin/com/legacycode/eureka/dex/ApkParser.kt
+++ dex/src/main/kotlin/com/legacycode/eureka/dex/ApkParser.kt
@@ -1,6 +1,7 @@
 package com.legacycode.eureka.dex
 
 import java.io.File
+import java.util.regex.Pattern
 import java.util.zip.ZipEntry
 import java.util.zip.ZipFile
 import org.jf.dexlib2.DexFileFactory
@@ -10,31 +11,41 @@ class ApkParser(override val file: File) : ArtifactParser {
   companion object {
     private const val DEX_FILE_EXTENSION = ".dex"
     private const val KITKAT = 19
+    private const val REGEX_ANONYMOUS_INNER_CLASS_SUFFIX = ".+\\$\\d;$"
   }
 
   override fun inheritanceAdjacencyList(): InheritanceAdjacencyList {
     val adjacencyList = InheritanceAdjacencyList()
 
     ZipFile(file).use { zipFile ->
-      val dexFileEntries = zipFile.entries().asSequence().filter(::dexFileFilter)
-      for (dexFileEntry in dexFileEntries) {
-        val dexEntry = DexFileFactory.loadDexEntry(
-          file,
-          dexFileEntry.name,
-          true,
-          Opcodes.forApi(KITKAT),
-        )
-
-        for (classDef in dexEntry.dexFile.classes) {
-          val classType = classDef.type
-          val superclassType = classDef.superclass
-
-          adjacencyList.add(Ancestor(superclassType!!), Child(classType))
-        }
-      }
+      parseDexClassFiles(zipFile, adjacencyList)
     }
 
     return adjacencyList
+  }
+
+  private fun parseDexClassFiles(
+    zipFile: ZipFile,
+    outAdjacencyList: InheritanceAdjacencyList,
+  ) {
+    val dexFileEntries = zipFile.entries().asSequence().filter(::dexFileFilter)
+    for (dexFileEntry in dexFileEntries) {
+      val dexEntry = DexFileFactory.loadDexEntry(
+        file,
+        dexFileEntry.name,
+        true,
+        Opcodes.forApi(KITKAT),
+      )
+
+      for (classDef in dexEntry.dexFile.classes) {
+        val classType = classDef.type
+        val superclassType = classDef.superclass
+
+        if (!Pattern.matches(REGEX_ANONYMOUS_INNER_CLASS_SUFFIX, classType)) {
+          outAdjacencyList.add(Ancestor(superclassType!!), Child(classType))
+        }
+      }
+    }
   }
 
   private fun dexFileFilter(entry: ZipEntry): Boolean {
--- dex/src/test/kotlin/com/legacycode/eureka/dex/ApkParserTest.kt
+++ dex/src/test/kotlin/com/legacycode/eureka/dex/ApkParserTest.kt
@@ -5,10 +5,11 @@ import org.approvaltests.Approvals
 import org.junit.jupiter.api.Test
 
 class ApkParserTest {
+  private val apkFile = TestApk("wikipedia.apk").file
+
   @Test
   fun `it can build an inheritance adjacency list from an APK`() {
     // given
-    val apkFile = TestApk("wikipedia.apk").file
     val parser = ApkParser(apkFile)
     val adjacencyList = parser.inheritanceAdjacencyList()
 
@@ -16,6 +17,22 @@ class ApkParserTest {
     val graphvizTree = adjacencyList.tree(
       Ancestor("Landroid/app/Activity;"),
       DotTreeBuilder("Wikipedia Activities"),
+    )
+
+    // then
+    Approvals.verify(graphvizTree)
+  }
+
+  @Test
+  fun `it should ignore anonymous inner classes`() {
+    // given
+    val adjacencyList = ApkParser(apkFile)
+      .inheritanceAdjacencyList()
+
+    // when
+    val graphvizTree = adjacencyList.tree(
+      Ancestor("Ljava/lang/Object;"),
+      DotTreeBuilder("Everything, all at once!"),
     )
 
     // then

a13618d0de94349af099324ddce5a5dfb4f9244d
Ragunath Jawahar <ragunath@redgreen.io>
2023-07-15 21:14:33 +0530

    test: add missing test for `Title`

---
+++ cli/src/test/kotlin/com/legacycode/eureka/hierarchy/TitleTest.kt
@@ -0,0 +1,16 @@
+package com.legacycode.eureka.hierarchy
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+
+class TitleTest {
+  @Test
+  fun displayText() {
+    // given
+    val title = Title("junit-runtime.jar", "org.junit.jupiter.api.Test")
+
+    // when & then
+    assertThat(title.displayText)
+      .isEqualTo("junit-runtime.jar (Test)")
+  }
+}

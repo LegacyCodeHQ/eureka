ff1d8ef42994ee2f2c866a23cd7bce461c0ed894
Ragunath Jawahar <ragunath@redgreen.io>
2023-07-15 21:12:40 +0530

    test: add tests for `Heading`

---
+++ cli/src/test/kotlin/com/legacycode/eureka/hierarchy/HeadingTest.kt
@@ -0,0 +1,26 @@
+package com.legacycode.eureka.hierarchy
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+
+class HeadingTest {
+  @Test
+  fun `without prune keyword`() {
+    // given
+    val heading = Heading("wikipedia.apk", "java.lang.Object", null)
+
+    // when & then
+    assertThat(heading.displayText)
+      .isEqualTo("wikipedia.apk (java.lang.Object)")
+  }
+
+  @Test
+  fun `with prune keyword`() {
+    // given
+    val heading = Heading("wikipedia.apk", "java.lang.Object", "Table")
+
+    // when & then
+    assertThat(heading.displayText)
+      .isEqualTo("""wikipedia.apk (java.lang.Object → showing "Table")""")
+  }
+}

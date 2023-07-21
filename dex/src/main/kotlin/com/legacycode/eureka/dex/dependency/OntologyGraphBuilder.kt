373c258ccec1cbec936083747b06409b527b56a6
Ragunath Jawahar <ragunath@redgreen.io>
2023-07-21 19:26:36 +0530

    feat: modify graph builder to create hierarchical data

--- dex/src/main/kotlin/com/legacycode/eureka/dex/dependency/OntologyGraphBuilder.kt
+++ dex/src/main/kotlin/com/legacycode/eureka/dex/dependency/OntologyGraphBuilder.kt
@@ -1,6 +1,7 @@
 package com.legacycode.eureka.dex.dependency
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ArrayNode
 import com.legacycode.eureka.dex.AdjacencyList
 import com.legacycode.eureka.dex.Ancestor
 import com.legacycode.eureka.dex.Child
@@ -8,7 +9,10 @@ import com.legacycode.eureka.dex.Node
 
 class OntologyGraphBuilder : AdjacencyList.GraphBuilder<String> {
   private val objectMapper = ObjectMapper()
-  private val rootNode = objectMapper.createArrayNode()
+  private val rootNode = objectMapper.createObjectNode()
+  private val linksArray = objectMapper.createArrayNode()
+  private val allNodes = mutableSetOf<String>()
+
   private lateinit var currentNode: Node
 
   override val out: String
@@ -17,15 +21,31 @@ class OntologyGraphBuilder : AdjacencyList.GraphBuilder<String> {
       .writeValueAsString(rootNode)
 
   override fun visitAncestor(ancestor: Ancestor) {
+    allNodes.add(ancestor.fqn)
     currentNode = ancestor
   }
 
   override fun visitChild(child: Child) {
+    allNodes.add(child.fqn)
+
     val route = objectMapper.createObjectNode().apply {
       put("source", currentNode.fqn)
       put("target", child.fqn)
     }
+    linksArray.add(route)
+  }
 
-    rootNode.add(route)
+  override fun afterTraversal() {
+    val nodesArray = objectMapper.createArrayNode()
+    allNodes.onEach {
+      val node = objectMapper.createObjectNode().apply {
+        put("id", it)
+        put("group", it.substring(0, it.lastIndexOf('.')))
+      }
+      nodesArray.add(node)
+    }
+
+    rootNode.set<ArrayNode>("nodes", nodesArray)
+    rootNode.set<ArrayNode>("links", linksArray)
   }
 }

b362b0d348ae863fa3f292c3a023ececb403f416
Ragunath Jawahar <ragunath@redgreen.io>
2023-07-22 22:13:53 +0530

    build: prepare next iteration

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -4,7 +4,7 @@ import java.util.Properties
 import org.jreleaser.model.Active
 
 @Suppress("PropertyName")
-val CLI_VERSION = "0.88.0"
+val CLI_VERSION = "0.89.0-SNAPSHOT"
 
 plugins {
   application

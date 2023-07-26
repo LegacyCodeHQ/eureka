b145738bade74782b8687e571d150e823abe486f
Ragunath Jawahar <ragunath@redgreen.io>
2023-07-26 12:07:37 +0530

    refactor: rename test class

--- web-server/src/test/kotlin/com/legacycode/eureka/web/ownership/OwnershipHtmlTest.kt
+++ web-server/src/test/kotlin/com/legacycode/eureka/web/ownership/OwnershipHtmlTest.kt
@@ -3,11 +3,11 @@ package com.legacycode.eureka.web.ownership
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
 
-class OwnershipServerTest {
+class OwnershipHtmlTest {
   @Test
   fun `it uses the bundled d3 library`() {
     // given
-    val ownershipHtml = OwnershipServerTest::class.java.classLoader
+    val ownershipHtml = OwnershipHtmlTest::class.java.classLoader
       .getResource("ownership.html")!!.readText()
 
     // when & then

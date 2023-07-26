780f2a55338d7b1a0fb2c5e22e2aabaf3f794dc8
Ragunath Jawahar <ragunath@redgreen.io>
2023-07-26 12:07:04 +0530

    refactor: move test

--- web-server/src/test/kotlin/com/legacycode/eureka/web/hierarchy/HierarchyHtmlTest.kt
+++ web-server/src/test/kotlin/com/legacycode/eureka/web/hierarchy/HierarchyHtmlTest.kt
@@ -1,5 +1,6 @@
 package com.legacycode.eureka.web.hierarchy
 
+import com.google.common.truth.Truth.assertThat
 import org.approvaltests.Approvals
 import org.junit.jupiter.api.Test
 
@@ -7,16 +8,31 @@ class HierarchyHtmlTest {
   @Test
   fun content() {
     // given
-    val filename = "junit-runtime.jar"
-    val className = "java.lang.Object"
+    val title = Title("junit-runtime.jar", "java.lang.Object")
+    val heading = Heading("junit-runtime.jar", "java.lang.Object", null)
     val jsonData = """{"name": "java.lang.Object"}"""
-
-    val hierarchyHtml = HierarchyHtml(Title(filename, className), Heading(filename, className, null), jsonData)
+    val hierarchyHtml = HierarchyHtml(title, heading, jsonData)
 
     // when
     val content = hierarchyHtml.content
 
     // then
     Approvals.verify(content)
+  }
+
+  @Test
+  fun `it uses the bundled d3 library`() {
+    // given
+    val title = Title("junit-runtime.jar", "java.lang.Object")
+    val heading = Heading("junit-runtime.jar", "java.lang.Object", null)
+    val jsonData = """{"name": "java.lang.Object"}"""
+    val hierarchyHtml = HierarchyHtml(title, heading, jsonData)
+
+    // when
+    val content = hierarchyHtml.content
+
+    // then
+    assertThat(content)
+      .contains("<script src=\"/static/js/d3.v7.min.js\"></script>")
   }
 }

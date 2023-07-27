d7150ad3538010890641e6f9f6afe4064c23a3f1
Ragunath Jawahar <ragunath@redgreen.io>
2023-07-27 19:03:39 +0530

    build: bump Kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
   id("com.github.ben-manes.versions") version ("0.42.0") apply false
   id("io.gitlab.arturbosch.detekt") version ("1.21.0") apply false
   id("com.github.node-gradle.node") version ("5.0.0") apply false
-  id("org.jetbrains.kotlinx.kover") version ("0.7.1") apply false
+  id("org.jetbrains.kotlinx.kover") version ("0.7.2") apply false
 }
 
 allprojects {

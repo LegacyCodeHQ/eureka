e0aa4ba61da0ae4cd7aae680e5fe898775d8c404
Ragunath Jawahar <ragunath@legacycode.com>
2025-02-11 10:10:32 +0530

    refactor: replace `toUpperCase` with `uppercase`

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ subprojects {
 }
 
 fun isStable(version: String): Boolean {
-  val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+  val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
   val regex = "^[0-9,.v-]+(-r)?$".toRegex()
   return stableKeyword || regex.matches(version)
 }
